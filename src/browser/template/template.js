4f5903b97a6d38d0eb2cf8d6308ca39cd78de150
Dominic Gannaway <dominic.gannaway@caplin.com>
2015-09-11 10:41:07 +0100

    Added eslint file

--- src/browser/template/template.js
+++ src/browser/template/template.js
@@ -3,11 +3,9 @@ import addProperties from "./addProperties";
 import isBrowser     from "../../util/isBrowser";
 
 export default {
-
     addAttributes: addAttributes,
     addProperties: addProperties,
     createElement: (tag) => {
-
         if (isBrowser) {
             return document.createElement(tag);
         }
--- src/universal/core/attachFragmentList.js
+++ src/universal/core/attachFragmentList.js
@@ -1,11 +1,7 @@
 import attachFragment from "./attachFragment";
 
 export default ( context, list, parentDom, component ) => {
-
     for ( let i = 0; i < list.length; i++ ) {
-
         attachFragment( context, list[i], parentDom, component );
-
     }
-
 };
