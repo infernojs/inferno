3fae7364d4f4282454a3f2ad700ccc3c17b4bfa7
KFlash <kflash123@gmail.com>
2015-09-13 18:11:23 +0800

    SSR works now with object literal

--- src/browser/template/renderToString.js
+++ src/browser/template/renderToString.js
@@ -1,27 +1,92 @@
 import voidCfg        from "./cfg/voidCfg";
 import forIn          from "../../util/forIn";
+import inArray        from "../../util/inArray";
 import { SSRStyles }  from "./CSSOperations";
 import { attrToHtml } from "./DOMOperations";
 
-// WORK IN PROGRESS!!!!!!!
-function renderToString(props, tagName, children) {
+let ctx = null;
 
-    let attrType,
+function booleanAttrToString(name, value) {
+    return value ? name : "";
+}
+
+/**
+ * To get 'selected' and 'selected multiple' rendered correct, we need to do this
+ * 'little' test. Else highlighted select values will not be rendred.
+ * E.g. '<option selected value="2"></option>' will become '<option value="2"></option>'
+ */
+
+function createMarkupForSelect(tagName, value, props, prop) {
+
+    let html = "";
+
+    if (tagName === "select") {
+
+        ctx = {
+            value: value,
+            multiple: props.multiple
+        };
+
+    } else if (tagName === "option") {
+
+        if (ctx && (ctx.multiple ? inArray(ctx.value, value) : ctx.value === value)) {
+
+            html += " " + booleanAttrToString("selected", true) + " " + prop + "=\"" + "" + value + "\""
+
+        } else {
+
+            html += " " + prop + "=\"" + "" + value + "\"";
+        }
+    }
+    return html;
+}
+
+function renderToString(props, tagName) {
+
+    // Create a empty objects to avoid the same mistakes as React did
+    const properties = {};
+
+    let children = props.children || null;
+
+    // check if we have any attrs / props
+    if (props != null) {
+
+        for (key in props) {
+
+            if (key === "value") {
+
+                if (tagName === "textarea" || props.contenteditable) {
+
+                    // Child are a pure text string
+                    children = props[key];
+                    continue;
+                }
+            }
+            properties[key] = props[key];
+        }
+    }
+
+    let innerHTML = props && props.innerHTML,
         html = "<" + tagName,
         attr;
 
-    if (props != null) {
+    // try to create markup for HTML attributes and properties
+    if (properties != null) {
 
-        forIn(props, (name, value) => {
+        forIn(properties, (name, value) => {
+            if (name !== "innerHTML" && (value || (typeof value === "number"))) {
+                // Special case: "select" and "select multiple"
+                if (name === "value" && (tagName === "select" || tagName === "option")) {
+                    html += createMarktupForSelect(tagName, value, props, name);
+                } else if (name === "style") {
+                    html += " " + name + "=\"" + "" + SSRStyles(value) + "\"";
 
-            if (name === "style") {
-                html += " " + name + "=\"" + "" + SSRStyles(value) + "\"";
-
-            // we need to check for number values, else expected - '<a download="0"></a>' - would
-            // become - '<a></a>'. And the '0' - zero - will be skipped.
-            } else if (name !== "innerHTML" && (value || (typeof value === "number"))) {
-                if (value !== "false") {
-                    html += " " + attrToHtml(name, value);
+                    // we need to check for number values, else expected - '<a download="0"></a>' - would
+                    // become - '<a></a>'. And the '0' - zero - will be skipped.
+                } else if (name !== "innerHTML" && (value || (typeof value === "number"))) {
+                    if (value !== "false") {
+                        html += " " + attrToHtml(name, value);
+                    }
                 }
             }
         });
@@ -31,11 +96,27 @@ function renderToString(props, tagName, children) {
 
         html = html + "/>";
 
+        // child nodes
     } else {
 
         html = html + ">";
 
         // ... child nodes
+        if (children && children.length) {
+
+            let index = 0;
+
+            const len = children.length;
+
+            for (; index < len; index++) {
+
+                html += renderToString(children[index], tagName);
+            }
+
+        } else if (innerHTML) {
+
+            html += innerHTML;
+        }
 
         html += "</" + tagName + ">";
     }
