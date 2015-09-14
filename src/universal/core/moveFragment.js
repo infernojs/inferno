67343c9889dcf468664c38a08453eca85432690b
KFlash <kflash123@gmail.com>
2015-09-14 22:59:53 +0800

    cleaning up moveFragment.js

--- src/universal/core/moveFragment.js
+++ src/universal/core/moveFragment.js
@@ -1,5 +1,3 @@
-import isBrowser from "../../util/isBrowser";
-
 export default (parentDom, item, nextItem) => {
 
     let domItem = item.dom,
@@ -7,9 +5,7 @@ export default (parentDom, item, nextItem) => {
 
     if (domItem !== domRefItem) {
 
-//        if (isBrowser()) {
-            let activeFragment = document.activeElement;
-//        }
+        let activeFragment = document.activeElement;
 
         if (domRefItem) {
 
@@ -20,16 +16,8 @@ export default (parentDom, item, nextItem) => {
             parentDom.appendChild(domItem);
         }
 
-        /**
-         * Same as document.activeElement but wraps in a try-catch block. In IE it is
-         * not safe to call document.activeElement if there is nothing focused.
-         *
-         * The activeElement will be null only if the document body is not yet defined.
-         */
-//        if (isBrowser()) {
-            if (activeFragment != document.body && (document.activeElement !== activeFragment)) {
-                activeDomNode.focus();
-            }
-  //      }
+        if (activeFragment != document.body && (document.activeElement !== activeFragment)) {
+            activeDomNode.focus();
+        }
     }
 };
