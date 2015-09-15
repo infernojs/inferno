34e2521896d170a1dfe6b43f8e095cd2557cfba8
KFlash <kflash123@gmail.com>
2015-09-15 23:15:17 +0800

    added a few new tests

--- tests/tests.js
+++ tests/tests.js
@@ -3,7 +3,10 @@ var Inferno = require('../src/inferno');
 var chai = require('chai');
 var t7 = require('../examples/t7');
 var isBrowser = require('../src/util/isBrowser');
+var DOMOperations = require('../src/browser/template/DOMOperations');
+var addAttributes = require('../src/browser/template/addAttributes');
 var expect = chai.expect;
+var setHtml = DOMOperations.setHtml;
 
 //expose t7 and Inferno globally
 global.t7 = t7;
@@ -458,5 +461,159 @@ describe('Inferno acceptance tests', function() {
 				expect(test).to.equal(expected);
 			});
 		});
+	});
+
+	describe('DOM operations', function() {
+  	  
+	  var container;
+
+	beforeEach(function() {
+	    container = document.createElement('div');
+	});
+
+   describe('.setHtml()', function() {
+	it("should render 'checked' as a property", function () {
+	    setHtml(container, "checked", true);
+	    expect(container.checked).to.equal(true);
+	});
+
+	it("should support custom attributes", function () {
+	    setHtml(container,  "custom-attr", 123);
+	   expect( container.getAttribute( "custom-attr" ) ).to.equal( "123" );
+	});
+
+	it("shouldn\'t render null values", function () {
+	    setHtml(container,  "value", null);
+        expect( container.value ).to.be.undefined;
+	});
+
+//	it("should set a 'id' attribute", function () {
+//	    setHtml(container,  "custom-attr", 123)
+//	    expect( container.id).to.equal( "foo" );
+		
+	//});
+
+	it("should set 'title' attribute", function () {
+	    setHtml(container,  "title", "dominic");
+	    expect( container.getAttribute( "title" ) ).to.equal( "dominic" );
+		
+	});
+
+	it("should support HTML5 data-* attribute", function () {
+	    setHtml(container, "data-foo", "bar");
+	    expect( container.getAttribute( "data-foo") ).to.equal( "bar" );
+		
+	});
+
+	it("should handle checkbox", function () {
+	    setHtml(container, "data-foo", "bar");
+	    expect( container.getAttribute( "data-foo") ).to.equal( "bar" );
+		
+	});
+
+});
+
+        describe('.addAttributes()', function() {
+
+    it( "should handle radio buttons", function () {
+	    
+		addAttributes(container, { type: "radio", checked: true });
+        expect( container.getAttribute( "type" ) ).to.eql( "radio" );
+        expect( container.checked ).to.be.true;
+
+    });
+	
+	it( "should set the 'title' attribute", function () {
+
+		addAttributes(container, { title: "FooBar" });
+        expect( container.getAttribute( "title" ) ).to.eql( "FooBar" );
+
+    } );
+	 
+    it( "should handle empty attributes", function () {
+        addAttributes(container, {});
+        expect( container.attrs ).to.be.undefined;
+    } );
+	
+	 it( "should ignore a `undefined` attribute", function () {
+
+        addAttributes(container, { "class": undefined })
+        expect( container.hasAttribute( "class" ) ).to.be.false;
+
+    } );
+
+    it( "should ignore a `undefined` value attribute", function () {
+        addAttributes(container, { "value": undefined });
+        expect( "value" in container ).to.be.false;
+        expect( container.hasAttribute( "value" ) ).to.be.false;
+
+    } );
+	
+	 it( "should set 'selectedIndex' property as an attribute", function () {
+        addAttributes(container, { type: "option", selectedIndex: true });
+        expect( container.getAttribute( "type" ) ).to.eql( "option");
+
+    } );
+
+    it( "should set 'selected' boolean attribute", function () {
+        addAttributes(container, { type: "radio", selected: true });
+        expect( container.getAttribute( "type" ) ).to.eql( "radio" );
+        expect( container.selected ).to.be.true;
+
+    } );
+
+    it( "should set 'selected' boolean attribute as selected", function () {
+        addAttributes(container, { type: "radio", selected: "selected" });
+        expect( container.getAttribute( "type" ) ).to.eql( "radio" );
+        expect( container.selected ).to.eql("selected");
+
+    } );
+	 
+    it( "should set 'checked' boolean attribute", function () {
+
+        addAttributes(container, { type: "checkbox", checked: true });
+        expect( container.getAttribute( "type" ) ).to.eql( "checkbox" );
+       expect( container.checked ).to.be.true;
+
+    } );
+
+   it( "should set 'checked' boolean attribute as checked", function () {
+      addAttributes(container, { type: "checkbox", checked: "checked"});
+      expect( container.getAttribute( "type" ) ).to.eql( "checkbox" );
+      expect( container.checked).to.eql( "checked" );
+
+    } );
+
+    it( "should set 'disabled' boolean attribute as disabled", function () {
+        
+		addAttributes(container, { type: "radio", disabled: "disabled" } );
+        expect( container.getAttribute( "type" ) ).to.eql( "radio" );
+        expect( container.getAttribute("disabled") ).to.eql( "" );
+
+    } );
+
+    it( "should set 'open' attribute", function () {
+        addAttributes(container, { open: "open" } );
+        expect( container.open ).to.eql( "open" );
+    } );	
+
+    it( "should set 'contenteditable' attribute", function () {
+        addAttributes(container, { contenteditable: true  } );
+        expect( container.getAttribute( "contenteditable" ) ).to.eql( "true" );
+
+    } );	
+
+    it( "should set 'maxlength' attribute", function () {
+        addAttributes(container, { maxlength: "5" } );
+        expect( container.getAttribute( "maxlength" ) ).to.eql( "5" );
+
+    } );	
+
+    it( "should set 'aria-disabled' attribute", function () {
+        addAttributes(container, { "aria-disabled": false } );
+        expect( container.getAttribute( "aria-disabled" ) ).to.eql( "false" );
+    });		
+  });
+
 	});
 });
