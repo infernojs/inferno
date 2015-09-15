bfd0e9cfd3ca6630fb1300efdc314b8c5771dc38
edge <sort@programmer.net>
2015-09-15 16:36:40 +0900

    update package.json

--- src/universal/core/updateFragmentList.js
+++ src/universal/core/updateFragmentList.js
@@ -22,11 +22,11 @@ export default function updateFragmentList( context, oldList, list, parentDom, c
 		return;
 	}
 
-	 if (oldListLength === 1 && listLength === 1) {
-		 if ( oldList[0].key === list[0].key) {
-		  updateFragment(context, oldList[0], list[0], parentDom);
-		return;
-	  }
+	if (oldListLength === 1 && listLength === 1) {
+		if (oldList[0].key === list[0].key) {
+			updateFragment(context, oldList[0], list[0], parentDom);
+			return;
+		}
 	}
 
 	let oldEndIndex = oldListLength - 1;
