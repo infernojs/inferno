5e59fa1f7eba7d6ee09727c8f3f347a996b08b6c
KFlash <kflash123@gmail.com>
2015-09-17 19:00:38 +0800

    optimized

--- src/core/createFragment.js
+++ src/core/createFragment.js
@@ -2,7 +2,7 @@ import minErr  from '../util/minErr';
 import isArray from '../util/isArray';
 
 export default function createFragment(values, template) {
-	if (template.key === undefined) {
+	if (template.key == null) {
 		minErr('createFragment()', 'Template is missing a key');
 	}
 
@@ -13,11 +13,8 @@ export default function createFragment(values, template) {
 		template: template,
 		templateTypes: null
 	};
-	/**
-	 * To 'failsafe' this and avoid throwing on non-constructor, we have to check
-	 * if the 'values' are 'null'.
-	 */
-	if (values !== null && isArray(values)) {
+
+	if (values != null && (isArray(values))) {
 		fragmentObject.templateElements = null;
 		fragmentObject.templateValues = values;
 	} else {
