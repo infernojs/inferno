6e13b3b190f902df78304354d12fdf73e4f90e18
KFlash <kflash123@gmail.com>
2015-09-27 06:23:51 +0800

    optimized element creation

--- src/template/index.js
+++ src/template/index.js
@@ -3,25 +3,42 @@ import addAttributes        from './addAttributes';
 import extendUnitlessNumber from './extendUnitlessNumber';
 import AttributeOps         from './AttributeOps';
 
+let svgNamespace = 'http://www.w3.org/2000/svg';
+let mathNamespace = 'http://www.w3.org/1998/Math/MathML';
+
 export default {
     addAttributes,
     extendUnitlessNumber,
     AttributeOps,
-    createElement: (tag, xmlns, is, parent) => {
+    createElement: (tag, xmlns, is) => {
 
-            let element, namespace;
+            let element;
 
-            if ( xmlns ) {
-                element = document.createElementNS("http://www.w3.org/2000/svg", tag, is);
-			} if (svgCfg[tag]) {
-               element = document.createElementNS("http://www.w3.org/2000/svg", tag, is);
-            } else if ( tag === 'math') {
-				element = document.createElementNS('http://www.w3.org/1998/Math/MathML', tag);
-			} else {
-                    element = document.createElement(tag);
+            // div, span, img, href
+            if (tag === 'div' ||
+                tag === 'span' ||
+                tag === 'img' ||
+                tag === 'href') {
+
+                return document.createElement(tag);
+            }
+            
+			// provided namespace given by the end-devs
+            if (xmlns) {
+                return document.createElementNS(xmlns, tag, is);
+            }
+            
+			// mathML namespace
+            if (tag === 'math') {
+                return document.createElementNS(mathNamespace, tag, is);
+            }
+            
+			// SVG elements supported by Inferno
+            if (svgCfg[tag]) {
+                return document.createElementNS(svgNamespace, tag, is);
             }
 
-            return element;
+            return document.createElement(tag);
 
         },
         createTextNode: text => document.createTextNode(text),
