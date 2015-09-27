8f0e4a726f209c102361bbb2aa10516f8d8f72b0
KFlash <kflash123@gmail.com>
2015-09-27 19:43:09 +0800

    optimized most common element tags

--- src/template/createElementWithIs.js
+++ src/template/createElementWithIs.js
@@ -7,44 +7,32 @@ function createElementWithIs(tag, xmlns, is) {
     switch (tag) {
         case "a":
             return document.createElement('a', is);
-        case 'body':
-            return document.createElement('body', is);
-        case 'br':
-            return document.createElement('br', is);
         case 'button':
             return document.createElement('button', is);
-        case 'circle':
-            return document.createElementNS(svgNamespace, 'circle', is);
         case 'div':
             return document.createElement('div', is);
         case 'em':
             return document.createElement('em', is);
         case 'form':
             return document.createElement('form', is);
-        case 'href':
-            return document.createElement('href', is);
         case 'img':
             return document.createElement('img', is);
-        case 'option':
-            return document.createElement('option', is);
+        case 'h1':
+            return document.createElement('h1', is);
+        case 'h2':
+            return document.createElement('h2', is);
         case 'li':
             return document.createElement('li', is);
         case 'ol':
             return document.createElement('ol', is);
-        case 'rec':
-            return document.createElementNS(svgNamespace, 'rec', is);
-        case 'select':
-            return document.createElement('select', is);
+        case 'p':
+            return document.createElement('p', is);
         case 'span':
             return document.createElement('span', is);
-        case 'style':
-            return document.createElement('style', is);
-        case 'table':
-            return document.createElement('table', is);
         case 'ul':
             return document.createElement('ul', is);
-        case 'elipse':
-            return document.createElementNS(svgNamespace, 'elipse', is);
+        case 'svg':
+            return document.createElementNS(svgNamespace, 'svg', is);
         default:
 
             if (xmlns) {
--- src/template/createElementWithoutIs.js
+++ src/template/createElementWithoutIs.js
@@ -7,44 +7,32 @@ function createElementWithoutIs(tag, xmlns) {
     switch (tag) {
         case "a":
             return document.createElement('a');
-        case 'body':
-            return document.createElement('body');
-        case 'br':
-            return document.createElement('br');
         case 'button':
             return document.createElement('button');
-        case 'circle':
-            return document.createElementNS(svgNamespace, 'circle');
         case 'div':
             return document.createElement('div');
         case 'em':
             return document.createElement('em', is);
         case 'form':
             return document.createElement('form');
-        case 'href':
-            return document.createElement('href');
         case 'img':
             return document.createElement('img');
-        case 'option':
-            return document.createElement('option');
+        case 'h1':
+            return document.createElement('h1', is);
+        case 'h2':
+            return document.createElement('h2', is);
         case 'li':
             return document.createElement('li');
         case 'ol':
             return document.createElement('ol');
-        case 'rec':
-            return document.createElementNS(svgNamespace, 'rec');
-        case 'select':
-            return document.createElement('select');
+        case 'p':
+            return document.createElement('p');
         case 'span':
             return document.createElement('span');
-        case 'style':
-            return document.createElement('style');
-        case 'table':
-            return document.createElement('table');
         case 'ul':
             return document.createElement('ul');
-        case 'elipse':
-            return document.createElementNS(svgNamespace, 'elipse');
+        case 'svg':
+            return document.createElementNS(svgNamespace, 'svg');
         default:
             if (xmlns) {
                 return document.createElementNS(xmlns, tag);
