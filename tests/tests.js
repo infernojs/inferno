3fc84358db8ff3cdf3733e7a463e5896d7dd8057
Dominic Gannaway <dominic@101010digital.com>
2015-10-04 12:31:21 +0100

    renamed folder to acceptance

--- tests/tests.js
+++ tests/tests.js
@@ -4,17 +4,17 @@ import Inferno from '../src';
 import { expect } from 'chai';
 
 //render test group
-import renderSvgTests from './groups/render/svg-tests';
-import renderMathMlTests from './groups/render/math-ml-tests';
-import renderDomElementsTests from './groups/render/dom-elements-tests';
-import renderVirtualElementsTests from './groups/render/virtual-elements-tests';
+import renderSvgTests from './acceptance/render/svg-tests';
+import renderMathMlTests from './acceptance/render/math-ml-tests';
+import renderDomElementsTests from './acceptance/render/dom-elements-tests';
+import renderVirtualElementsTests from './acceptance/render/virtual-elements-tests';
 
 //renderToString test group
-import renderToStringDomElementsTests from './groups/renderToString/dom-elements-tests';
-import renderToStringVirtualElementsTests from './groups/renderToString/virtual-elements-tests';
+import renderToStringDomElementsTests from './acceptance/renderToString/dom-elements-tests';
+import renderToStringVirtualElementsTests from './acceptance/renderToString/virtual-elements-tests';
 
-import domOperationTests from './groups/dom-operation-tests';
-import cssOperationTests from './groups/css-operation-tests';
+import domOperationTests from './acceptance/dom-operation-tests';
+import cssOperationTests from './acceptance/css-operation-tests';
 
 describe('Inferno acceptance tests', () => {
 	describe('Inferno.render()', () => {
