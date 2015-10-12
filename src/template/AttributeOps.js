80f9428a8675957e1aa3cb6a49e678600bf1ae62
KFlash <kflash123@gmail.com>
2015-10-12 15:17:33 +0800

    select / select multiple bug fix
    - didn't handle numbers correctly

--- src/template/AttributeOps.js
+++ src/template/AttributeOps.js
@@ -217,15 +217,30 @@ let setValueForProperty = (node, name, value) => {
  */
 
 let setSelectValue = (node, value) => {
-
+	
 	const multiple = isArray(value),
 		options = node.options;
 
+if ( multiple && (typeof value[0] === 'number')) {
+ let selectedValue = {};
+    for (i = 0; i < value.length; i++) {
+      selectedValue['' + value[i]] = true;
+    }
+
+    for (i = 0; i < options.length; i++) {
+      var selected = selectedValue.hasOwnProperty(options[i].value);
+      if (options[i].selected !== selected) {
+        options[i].selected = true;
+      }
+    }
+
+}	 else {
 	let optionNode;
 	for (let i = 0; i < options.length; i++) {
 		optionNode = options[i];
 		optionNode.selected = value != null && (multiple ? inArray(value, optionNode.value) : optionNode.value == value);
 	}
+}
 };
 
 /**
