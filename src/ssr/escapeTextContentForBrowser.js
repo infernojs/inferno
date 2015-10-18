b8e5b614facb73a57627f23cb7419c86b76563b0
KFlash <kflash123@gmail.com>
2015-10-19 06:49:23 +0800

    improved escaping of html / attrs

--- src/ssr/escapeTextContentForBrowser.js
+++ src/ssr/escapeTextContentForBrowser.js
@@ -1,26 +1,17 @@
-/**
- * Escapes attribute value to prevent scripting attacks.
- *
- * @param {*} value Value to escape.
- * @return {string} An escaped string.
- */
-
 let ESCAPE_LOOKUP = {
   '&': '&amp;',
   '>': '&gt;',
-  '<': '&lt;',
-  '"': '&quot;',
-  '\'': '&#x27;',
+  '<': '&lt;'
 };
 
-let ESCAPE_REGEX = /[&><"']/g;
+let ESCAPE_REGEX = /[&><]/g;
 
 let escaper = (match) => ESCAPE_LOOKUP[match];
 
 /**
  * Escapes text to prevent scripting attacks.
  *
- * @param {*} text Text value to escape.
+ * @param {*} text content value to escape.
  * @return {string} An escaped string.
  */
 export default (text) => ('' + text).replace(ESCAPE_REGEX, escaper);
--- src/ssr/quoteAttributeValueForBrowser.js
+++ src/ssr/quoteAttributeValueForBrowser.js
@@ -7,3 +7,23 @@ import escapeTextContentForBrowser from './escapeTextContentForBrowser';
  * @return {string} An escaped string.
  */
 export default (value) => '"' + escapeTextContentForBrowser(value) + '"';
+
+
+let ESCAPE_LOOKUP = {
+ '&': '&amp;',
+  '>': '&gt;',
+  '<': '&lt;',
+  '"': '&quot;'
+};
+
+let ESCAPE_REGEX = /[&><"]/g;
+
+let escaper = (match) => ESCAPE_LOOKUP[match];
+
+/**
+ * Escapes attribute value to prevent scripting attacks.
+ *
+ * @param {*} value Attribute value to escape.
+ * @return {string} An escaped string.
+ */
+export default (value) => '"' + ('' + value).replace(ESCAPE_REGEX, escaper) + '"';
