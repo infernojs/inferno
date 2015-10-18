073aee28c719cf9b22b35fa61c56d00dbe561217
KFlash <kflash123@gmail.com>
2015-10-18 13:56:08 +0800

    minor adjustments

--- src/template/DOMProperty.js
+++ src/template/DOMProperty.js
@@ -2,15 +2,14 @@ import DOMAttributeNamespaces from './vars/DOMAttributeNamespaces';
 import checkMask              from './checkMask';
 
 let
-    MUST_USE_ATTRIBUTE = 0x1,
-    MUST_USE_PROPERTY = 0x2,
-    HAS_BOOLEAN_VALUE = 0x8,
-    HAS_NUMERIC_VALUE = 0x10,
-    HAS_POSITIVE_NUMERIC_VALUE = 0x20 | 0x10,
-    HAS_OVERLOADED_BOOLEAN_VALUE = 0x40,
+    MustUseAttribute = 0x1,
+    MustUseProperty = 0x2,
+    HasBooleanValue = 0x8,
+    HasNumericValue = 0x10,
+    HasPositiveNumber = 0x20 | 0x10,
+    HasOverloadedBooleanValue = 0x40;
 
-    HTMLDOMPropertyConfig = {
-        Properties: {
+let  Properties = {
             /**
              * Standard Properties
              */
@@ -18,106 +17,106 @@ let
             acceptCharset: null,
             accessKey: null,
             action: null,
-            allowFullScreen: MUST_USE_ATTRIBUTE | HAS_BOOLEAN_VALUE,
-            allowTransparency: MUST_USE_ATTRIBUTE,
+            allowFullScreen: MustUseAttribute | HasBooleanValue,
+            allowTransparency: MustUseAttribute,
             alt: null,
-            async: HAS_BOOLEAN_VALUE,
+            async: HasBooleanValue,
             autoComplete: null,
-            autoFocus: HAS_BOOLEAN_VALUE,
-            autoPlay: HAS_BOOLEAN_VALUE,
-            capture: MUST_USE_ATTRIBUTE | HAS_BOOLEAN_VALUE,
+            autoFocus: HasBooleanValue,
+            autoPlay: HasBooleanValue,
+            capture: MustUseAttribute | HasBooleanValue,
             cellPadding: null,
             cellSpacing: null,
-            charSet: MUST_USE_ATTRIBUTE,
-            challenge: MUST_USE_ATTRIBUTE,
-            checked: MUST_USE_PROPERTY | HAS_BOOLEAN_VALUE,
-            classID: MUST_USE_ATTRIBUTE,
+            charSet: MustUseAttribute,
+            challenge: MustUseAttribute,
+            checked: MustUseProperty | HasBooleanValue,
+            classID: MustUseAttribute,
             // To set className on SVG elements, it's necessary to use .setAttribute;
             // this works on HTML elements too in all browsers except IE8.
-            className: MUST_USE_ATTRIBUTE,
-            cols: MUST_USE_ATTRIBUTE | HAS_POSITIVE_NUMERIC_VALUE,
+            className: MustUseAttribute,
+            cols: MustUseAttribute | HasPositiveNumber,
             colSpan: null,
             content: null,
             contentEditable: null,
-            contextMenu: MUST_USE_ATTRIBUTE,
-            controls: MUST_USE_PROPERTY | HAS_BOOLEAN_VALUE,
+            contextMenu: MustUseAttribute,
+            controls: MustUseProperty | HasBooleanValue,
             coords: null,
             crossOrigin: null,
             data: null, // For `<object />` acts as `src`.
-            dateTime: MUST_USE_ATTRIBUTE,
-            defer: HAS_BOOLEAN_VALUE,
+            dateTime: MustUseAttribute,
+            defer: HasBooleanValue,
             dir: null,
-            disabled: MUST_USE_ATTRIBUTE | HAS_BOOLEAN_VALUE,
-            download: MUST_USE_ATTRIBUTE,
+            disabled: MustUseAttribute | HasBooleanValue,
+            download: MustUseAttribute,
             draggable: null,
             encType: null,
-            form: MUST_USE_ATTRIBUTE,
-            formAction: MUST_USE_ATTRIBUTE,
-            formEncType: MUST_USE_ATTRIBUTE,
-            formMethod: MUST_USE_ATTRIBUTE,
-            formNoValidate: HAS_BOOLEAN_VALUE,
-            formTarget: MUST_USE_ATTRIBUTE,
-            frameBorder: MUST_USE_ATTRIBUTE,
+            form: MustUseAttribute,
+            formAction: MustUseAttribute,
+            formEncType: MustUseAttribute,
+            formMethod: MustUseAttribute,
+            formNoValidate: HasBooleanValue,
+            formTarget: MustUseAttribute,
+            frameBorder: MustUseAttribute,
             headers: null,
-            height: MUST_USE_ATTRIBUTE,
-            hidden: MUST_USE_ATTRIBUTE | HAS_BOOLEAN_VALUE,
+            height: MustUseAttribute,
+            hidden: MustUseAttribute | HasBooleanValue,
             high: null,
             href: null,
             hrefLang: null,
             htmlFor: null,
             httpEquiv: null,
             icon: null,
-            id: MUST_USE_PROPERTY,
-            is: MUST_USE_ATTRIBUTE,
-            keyParams: MUST_USE_ATTRIBUTE,
-            keyType: MUST_USE_ATTRIBUTE,
+            id: MustUseProperty,
+            is: MustUseAttribute,
+            keyParams: MustUseAttribute,
+            keyType: MustUseAttribute,
             label: null,
             lang: null,
-            list: MUST_USE_ATTRIBUTE,
-            loop: MUST_USE_PROPERTY | HAS_BOOLEAN_VALUE,
+            list: MustUseAttribute,
+            loop: MustUseProperty | HasBooleanValue,
             low: null,
-            manifest: MUST_USE_ATTRIBUTE,
+            manifest: MustUseAttribute,
             marginHeight: null,
             marginWidth: null,
             max: null,
-            maxLength: MUST_USE_ATTRIBUTE,
-            media: MUST_USE_ATTRIBUTE,
+            maxLength: MustUseAttribute,
+            media: MustUseAttribute,
             mediaGroup: null,
             method: null,
             min: null,
-            minLength: MUST_USE_ATTRIBUTE,
-            multiple: MUST_USE_ATTRIBUTE | HAS_BOOLEAN_VALUE,
-            muted: MUST_USE_PROPERTY | HAS_BOOLEAN_VALUE,
-            name: MUST_USE_ATTRIBUTE,
-            noValidate: HAS_BOOLEAN_VALUE,
-            open: HAS_BOOLEAN_VALUE,
+            minLength: MustUseAttribute,
+            multiple: MustUseAttribute | HasBooleanValue,
+            muted: MustUseProperty | HasBooleanValue,
+            name: MustUseAttribute,
+            noValidate: HasBooleanValue,
+            open: HasBooleanValue,
             optimum: null,
             pattern: null,
             placeholder: null,
             poster: null,
             preload: null,
             radioGroup: null,
-            readOnly: MUST_USE_PROPERTY | HAS_BOOLEAN_VALUE,
+            readOnly: MustUseProperty | HasBooleanValue,
             rel: null,
-            required: HAS_BOOLEAN_VALUE,
-            role: MUST_USE_ATTRIBUTE,
-            rows: MUST_USE_ATTRIBUTE | HAS_POSITIVE_NUMERIC_VALUE,
+            required: HasBooleanValue,
+            role: MustUseAttribute,
+            rows: MustUseAttribute | HasPositiveNumber,
             rowSpan: null,
             sandbox: null,
             scope: null,
-            scoped: HAS_BOOLEAN_VALUE,
+            scoped: HasBooleanValue,
             scrolling: null,
-            seamless: MUST_USE_ATTRIBUTE | HAS_BOOLEAN_VALUE,
-            selected: MUST_USE_PROPERTY | HAS_BOOLEAN_VALUE,
+            seamless: MustUseAttribute | HasBooleanValue,
+            selected: MustUseProperty | HasBooleanValue,
             shape: null,
-            size: MUST_USE_ATTRIBUTE | HAS_POSITIVE_NUMERIC_VALUE,
-            sizes: MUST_USE_ATTRIBUTE,
-            span: HAS_POSITIVE_NUMERIC_VALUE,
+            size: MustUseAttribute | HasPositiveNumber,
+            sizes: MustUseAttribute,
+            span: HasPositiveNumber,
             spellCheck: null,
-            src: MUST_USE_ATTRIBUTE,
-            srcDoc: MUST_USE_PROPERTY,
-            srcSet: MUST_USE_ATTRIBUTE,
-            start: MUST_USE_ATTRIBUTE | HAS_NUMERIC_VALUE,
+            src: MustUseAttribute,
+            srcDoc: MustUseProperty,
+            srcSet: MustUseAttribute,
+            start: MustUseAttribute | HasNumericValue,
             step: null,
             style: null,
             tabIndex: null,
@@ -125,9 +124,9 @@ let
             title: null,
             type: null,
             useMap: null,
-            value: MUST_USE_PROPERTY,
-            width: MUST_USE_ATTRIBUTE,
-            wmode: MUST_USE_ATTRIBUTE,
+            value: MustUseProperty,
+            width: MustUseAttribute,
+            wmode: MustUseAttribute,
 
             /**
              * Non-standard Properties
@@ -138,49 +137,50 @@ let
             autoCorrect: null,
             // itemProp, itemScope, itemType are for
             // Microdata support. See http://schema.org/docs/gs.html
-            itemProp: MUST_USE_ATTRIBUTE,
-            itemScope: MUST_USE_ATTRIBUTE | HAS_BOOLEAN_VALUE,
-            itemType: MUST_USE_ATTRIBUTE,
+            itemProp: MustUseAttribute,
+            itemScope: MustUseAttribute | HasBooleanValue,
+            itemType: MustUseAttribute,
             // itemID and itemRef are for Microdata support as well but
             // only specified in the the WHATWG spec document. See
             // https://html.spec.whatwg.org/multipage/microdata.html#microdata-dom-api
-            itemID: MUST_USE_ATTRIBUTE,
-            itemRef: MUST_USE_ATTRIBUTE,
+            itemID: MustUseAttribute,
+            itemRef: MustUseAttribute,
             // property is supported for OpenGraph in meta tags.
             property: null,
             // IE-only attribute that controls focus behavior
-            unselectable: MUST_USE_ATTRIBUTE
-        },
-        PropertyToAttributeMapping: {
+            unselectable: MustUseAttribute
+        };
+		
+ let PropertyToAttributeMapping = {
             'className': 'class',
             'htmlFor': 'for',
             'httpEquiv': 'http-equiv',
             'acceptCharset': 'accept-charset'
-        }
-    };
+        };
 
-//export default (node, styles) => {
+
 export default (() => {
     let propInfoByAttributeName = {};
+    
+	for(let propName in Properties) {
 
-    Object.keys(HTMLDOMPropertyConfig.Properties).forEach((propName) => {
-        let propConfig = HTMLDOMPropertyConfig.Properties[propName];
-        let attributeName = HTMLDOMPropertyConfig.PropertyToAttributeMapping[propName] || propName.toLowerCase();
+        let propConfig = Properties[propName];
+        let attributeName = PropertyToAttributeMapping[propName] || propName.toLowerCase();
 
         let propertyInfo = {
             attributeName: attributeName,
             propertyName: propName,
             attributeNamespace: null,
-            mustUseAttribute: checkMask(propConfig, MUST_USE_ATTRIBUTE),
-            mustUseProperty: checkMask(propConfig, MUST_USE_PROPERTY),
-            hasBooleanValue: checkMask(propConfig, HAS_BOOLEAN_VALUE),
-            hasNumericValue: checkMask(propConfig, HAS_NUMERIC_VALUE),
-            hasPositiveNumericValue: checkMask(propConfig, HAS_POSITIVE_NUMERIC_VALUE),
-            hasOverloadedBooleanValue: checkMask(propConfig, HAS_OVERLOADED_BOOLEAN_VALUE)
+            mustUseAttribute: checkMask(propConfig, MustUseAttribute),
+            mustUseProperty: checkMask(propConfig, MustUseProperty),
+            hasBooleanValue: checkMask(propConfig, HasBooleanValue),
+            hasNumericValue: checkMask(propConfig, HasNumericValue),
+            hasPositiveNumericValue: checkMask(propConfig, HasPositiveNumber),
+            hasOverloadedBooleanValue: checkMask(propConfig, HasOverloadedBooleanValue)
         };
 
         propInfoByAttributeName[attributeName] = propertyInfo;
-    });
+    }
 
     return (attributeName) => {
         let lowerCased = attributeName.toLowerCase();
