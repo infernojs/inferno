324140ea71af284a4a492eb8824a36bb15224e77
KFlash <kflash123@gmail.com>
2015-10-18 18:47:10 +0800

    removed SSR folder and SSR

--- src/ssr/quoteAttributeValueForBrowser.js
+++
@@ -1,9 +0,0 @@
-import escapeTextContentForBrowser from './escapeTextContentForBrowser';
-
-/**
- * Escapes attribute value to prevent scripting attacks.
- *
- * @param {*} value Value to escape.
- * @return {string} An escaped string.
- */
-export default (value) => '"' + escapeTextContentForBrowser(value) + '"';
