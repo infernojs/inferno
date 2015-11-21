5c0ddd2275c351abd2c05dd7563a27328d434792
KFlash <kflash123@gmail.com>
2015-11-21 16:55:32 +0800

    added a unused file

---
+++ src/template/updateDOMProperties.js
@@ -0,0 +1,66 @@
+import deleteValueForProperty from './deleteValueForProperty';
+import setValueForProperty from './setValueForProperty';
+import setValueForStyles from './setValueForStyles';
+import eventManager from '../events/eventManager';
+import events from '../events/shared/events';
+
+/**
+ * Detecting differences in property values and updating the DOM as necessary.
+ * This function is probably the single most critical path for performance optimization.
+ *
+ * @param {DOMElement} node
+ * @param {string} propKey
+ * @param {object} lastProps
+ * @param {object} nextProps
+ */
+function updateDOMProperties(element, propKey, lastProps, nextProps) {
+
+    if (propKey === 'style') {
+
+        let styleUpdates;
+        let styleName;
+
+        if (lastProps != null) {
+
+            if (nextProps == null) {
+                deleteValueForProperty(element, propKey);
+            } else {
+
+                // Unset styles on `lastProp` but not on `nextProp`.
+                for (styleName in lastProps) {
+                    if (lastProps[styleName] && (!nextProps || !nextProps[styleName])) {
+                        styleUpdates = styleUpdates || {};
+                        styleUpdates[styleName] = '';
+                    }
+                }
+                // Update styles that changed since `lastProps`.
+                for (styleName in nextProps) {
+                    if (nextProps[styleName] && lastProps[styleName] !== nextProps[styleName]) {
+                        styleUpdates = styleUpdates || {};
+                        styleUpdates[styleName] = nextProps[styleName];
+                    }
+                }
+            }
+        } else if (nextProps != null) {
+            styleUpdates = nextProps;
+        }
+
+        if (styleUpdates) {
+            setValueForStyles(element, styleUpdates);
+        }
+
+    } else {
+
+        if (events[propKey] != null) {
+            if (nextProps != null) {
+                eventManager.addListener(element, propKey, nextProps);
+            } else {
+                eventManager.removeListener(element, propKey);
+            }
+        } else {
+            setValueForProperty(element, propKey, nextProps);
+        }
+    }
+}
+
+export default updateDOMProperties;
