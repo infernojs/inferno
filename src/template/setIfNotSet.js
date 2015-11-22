fba9be29ac86f82ef435d021b7c667e0f8fcfc4b
KFlash <kflash123@gmail.com>
2015-11-22 09:22:20 +0800

    updated

--- src/template/setIfNotSet.js
+++ src/template/setIfNotSet.js
@@ -1,8 +1,28 @@
+function isInputProperty(tag, propName) {
+    switch (tag) {
+        case 'INPUT':
+            return propName === 'value' || propName === 'checked';
+        case 'TEXTAREA':
+            return propName === 'value';
+        case 'SELECT':
+            return propName === 'value' || propName === 'selectedIndex';
+        case 'OPTION':
+            return propName === 'selected';
+    }
+}
+
 function setIfNotSet(element, propName, value) {
 
-    if ('' + element[propName] !== '' + value) {
+    if (isInputProperty(element.tagName, propName)) {
+
+        if ('' + element[propName] !== '' + value) {
+            element[propName] = value;
+        }
+
+    } else {
         element[propName] = value;
     }
 }
 
 export default setIfNotSet;
+
