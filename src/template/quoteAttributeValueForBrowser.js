77e0bc31de1130173868bbf9b4f268cd0b3c76ae
KFlash <kflash123@gmail.com>
2015-11-24 06:42:32 +0800

    fixed quoteAttributeValueForBrowser

--- src/template/quoteAttributeValueForBrowser.js
+++ src/template/quoteAttributeValueForBrowser.js
@@ -1,13 +1,11 @@
-let ESCAPE_LOOKUP = {
+const ESCAPE_LOOKUP = {
  '&': '&amp;',
   '>': '&gt;',
   '<': '&lt;',
   '"': '&quot;'
 };
 
-let ESCAPE_REGEX = /[&><"]/g;
-
-let escaper = (match) => ESCAPE_LOOKUP[match];
+const ESCAPE_REGEX = /[&><"]/g;
 
 /**
  * Escapes attribute value to prevent scripting attacks.
@@ -15,4 +13,5 @@ let escaper = (match) => ESCAPE_LOOKUP[match];
  * @param {*} value Attribute value to escape.
  * @return {string} An escaped string.
  */
-export default (value) => '"' + ('' + value).replace(ESCAPE_REGEX, escaper) + '"';
+export default (value) => '"' + ('' + value).replace(ESCAPE_REGEX, (match) => ESCAPE_LOOKUP[match]) + '"';
+
