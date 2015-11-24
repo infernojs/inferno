dedbb915a07c16dfb08d69f09eb0f519e941d36e
KFlash <kflash123@gmail.com>
2015-11-24 08:58:20 +0800

    removed whitespace

--- src/template/dom/index.js
+++ src/template/dom/index.js
@@ -6,42 +6,42 @@ export default {
     addAttributes,
     createElement(tag, is) {
 
-            switch (tag) {
-                case 'div':
-                case 'span':
-                case 'button':
-                case 'em':
-                case 'form':
-                case 'img':
-                case 'a':
-                case 'ol':
-                case 'p':
-                case 'table':
-                case 'ul':
-                case 'ol':
-                case 'p':
+        switch (tag) {
+            case 'div':
+            case 'span':
+            case 'button':
+            case 'em':
+            case 'form':
+            case 'img':
+            case 'a':
+            case 'ol':
+            case 'p':
+            case 'table':
+            case 'ul':
+            case 'ol':
+            case 'p':
 
-				return createDOMNode(tag, false, is);
-				
-                default:
+                return createDOMNode(tag, false, is);
 
-                    let DOMElementsInfo = createDOMElements[tag.toLowerCase()];
+            default:
 
-                    if (DOMElementsInfo !== undefined) {
-                        // add SVG namespace
-                        if (DOMElementsInfo.isSVG) {
-                            return createDOMNode(tag, 'http://www.w3.org/2000/svg', is);
-                        }
-                        // add mathML namespace
-                        if (DOMElementsInfo.isMathML) {
-							return createDOMNode(tag, 'http://www.w3.org/1998/Math/MathML', is);
-                        }
+                let DOMElementsInfo = createDOMElements[tag.toLowerCase()];
+
+                if (DOMElementsInfo !== undefined) {
+                    // add SVG namespace
+                    if (DOMElementsInfo.isSVG) {
+                        return createDOMNode(tag, 'http://www.w3.org/2000/svg', is);
                     }
-                    // all others
-						return createDOMNode(tag, false, is);
-            }
-        },
-        createTextNode: text => document.createTextNode(text),
-        createEmptyText: () => document.createTextNode(''),
-        createEmptyDiv: () => document.createElement('div')
+                    // add mathML namespace
+                    if (DOMElementsInfo.isMathML) {
+                        return createDOMNode(tag, 'http://www.w3.org/1998/Math/MathML', is);
+                    }
+                }
+                // all others
+                return createDOMNode(tag, false, is);
+        }
+    },
+    createTextNode: text => document.createTextNode(text),
+    createEmptyText: () => document.createTextNode(''),
+    createEmptyDiv: () => document.createElement('div')
 };
