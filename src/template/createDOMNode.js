e557ae38c4a9406f9b1b7bed44d7acee69a1b9b1
KFlash <kflash123@gmail.com>
2015-11-24 18:09:08 +0800

    refix

--- src/template/createDOMNode.js
+++ src/template/createDOMNode.js
@@ -1,30 +1,27 @@
-const doc = global.document,
-    elementProtos = {};
+
+const doc = global.document;
 
 function createDOMNode(tagName, namespace, typextension) {
 
-    let element, key;
+    let element;
 
     if (namespace) {
         if (typextension) {
 
-            key = ns + is + ':' + tagName;
-            baseElement = elementProtos[key] || (elementProtos[key] = doc.createElementNS(namespace, tagName, typextension));
+            element = doc.createElementNS(namespace, tagName, typextension);
         } else {
 
-            key = ns + ':' + tagName;
-            baseElement = elementProtos[key] || (elementProtos[key] = doc.createElementNS(namespace, tagName));
+            element = doc.createElementNS(namespace, tagName);
         }
     } else {
         if (typextension) {
 
-            key = is + ':' + tagName;
-            element = elementProtos[key] || (elementProtos[key] = doc.createElement(tagName, typextension));
+            element = doc.createElement(tagName, typextension);
         } else {
-            element = elementProtos[tagName] || (elementProtos[tagName] = doc.createElement(tagName));
+            element = doc.createElement(tagName);
         }
     }
-    return element.cloneNode();;
+    return element;
 }
 
 export default createDOMNode;
