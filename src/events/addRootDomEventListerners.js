211db2a9744bf0c0d9b16e476d8bb4c4cd7e5535
Tom Jacques <tejacques@gmail.com>
2015-11-27 19:18:51 -0500

    Whitespace

--- src/events/addRootDomEventListerners.js
+++ src/events/addRootDomEventListerners.js
@@ -7,43 +7,43 @@ function addRootDomEventListerners(e, type) {
 
 	type || (type = e.type);
 
-    const cfg = EventRegistry[type];
+	const cfg = EventRegistry[type];
 
-    let target = e.target,
-        listenersCount = cfg.listenersCounter,
-        listeners,
-        listener,
-        domNodeId,
-        event = listenersCount > 0 && eventHooks(e);
+	let target = e.target,
+		listenersCount = cfg.listenersCounter,
+		listeners,
+		listener,
+		domNodeId,
+		event = listenersCount > 0 && eventHooks(e);
 
-    // NOTE: Only the event blubbling phase is modeled. This is done because
-    // handlers specified on props can not specify they are handled on the
-    // capture phase.
-    while(target !== null
-        && listenersCount > 0
-        && target !== document.parentNode) {
-        if(domNodeId = getDomNodeId(target, true)) {
-            listeners = listenersStorage[domNodeId];
-            if(listeners && (listener = listeners[type])) {
-                // 'this' on an eventListener is the element handling the event
-                // event.currentTarget is unwriteable, and since these are
-                // native events, will always refer to the document. Therefore
-                // 'this' is the only supported way of referring to the element
-                // whose listener is handling the current event
-                listener.call(target, event);
+	// NOTE: Only the event blubbling phase is modeled. This is done because
+	// handlers specified on props can not specify they are handled on the
+	// capture phase.
+	while(target !== null
+		&& listenersCount > 0
+		&& target !== document.parentNode) {
+		if(domNodeId = getDomNodeId(target, true)) {
+			listeners = listenersStorage[domNodeId];
+			if(listeners && (listener = listeners[type])) {
+				// 'this' on an eventListener is the element handling the event
+				// event.currentTarget is unwriteable, and since these are
+				// native events, will always refer to the document. Therefore
+				// 'this' is the only supported way of referring to the element
+				// whose listener is handling the current event
+				listener.call(target, event);
 
-                // Check if progagation stopped. There is only one listener per
-                // type, so we do not need to check immediate propagation.
-                if (event.isPropagationStopped()) {
-                    break;
-                }
+				// Check if progagation stopped. There is only one listener per
+				// type, so we do not need to check immediate propagation.
+				if (event.isPropagationStopped()) {
+					break;
+				}
 
-                --listenersCount;
-            }
-        }
+				--listenersCount;
+			}
+		}
 
-        target = target.parentNode;
-    }
+		target = target.parentNode;
+	}
 
 }
 
