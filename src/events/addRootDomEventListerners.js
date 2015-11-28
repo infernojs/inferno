2e269c5e7cca75c38dbb35cf75d548fccdcdb5f9
Tom Jacques <tejacques@gmail.com>
2015-11-27 19:18:31 -0500

    Updates to eventHook and addRootDomEventListeners logic

--- src/events/addRootDomEventListerners.js
+++ src/events/addRootDomEventListerners.js
@@ -4,23 +4,40 @@ import EventRegistry from './EventRegistry';
 import eventHooks from './eventHooks';
 
 function addRootDomEventListerners(e, type) {
-    
+
 	type || (type = e.type);
 
-    const cfg = EventRegistry[type],
-        listenersToInvoke = [];
+    const cfg = EventRegistry[type];
 
     let target = e.target,
         listenersCount = cfg.listenersCounter,
         listeners,
         listener,
-        domNodeId;
+        domNodeId,
+        event = listenersCount > 0 && eventHooks(e);
 
-    while(target !== null && listenersCount > 0 && target !== document.parentNode) {
+    // NOTE: Only the event blubbling phase is modeled. This is done because
+    // handlers specified on props can not specify they are handled on the
+    // capture phase.
+    while(target !== null
+        && listenersCount > 0
+        && target !== document.parentNode) {
         if(domNodeId = getDomNodeId(target, true)) {
             listeners = listenersStorage[domNodeId];
             if(listeners && (listener = listeners[type])) {
-                listenersToInvoke.push(listener);
+                // 'this' on an eventListener is the element handling the event
+                // event.currentTarget is unwriteable, and since these are
+                // native events, will always refer to the document. Therefore
+                // 'this' is the only supported way of referring to the element
+                // whose listener is handling the current event
+                listener.call(target, event);
+
+                // Check if progagation stopped. There is only one listener per
+                // type, so we do not need to check immediate propagation.
+                if (event.isPropagationStopped()) {
+                    break;
+                }
+
                 --listenersCount;
             }
         }
@@ -28,19 +45,6 @@ function addRootDomEventListerners(e, type) {
         target = target.parentNode;
     }
 
-    if(listenersToInvoke.length) {
-		const event = eventHooks(e);
-        const len = listenersToInvoke.length;
-
-        let i = 0;
-
-        while(i < len) {
-            listenersToInvoke[i++](event);
-            if(event.isPropagationStopped()) {
-                break;
-            }
-        }
-    }
 }
 
 export default addRootDomEventListerners;
--- src/events/eventHooks.js
+++ src/events/eventHooks.js
@@ -1,39 +1,77 @@
-let isPropagationStopped = false;
-let isDefaultPrevented = false;
+function stopPropagation() {
+    this._isPropagationStopped = true;
+    if (this._stopPropagation) {
+        this._stopPropagation();
+    } else {
+        this.cancelBubble = true;
+    }
+};
+
+function isPropagationStopped() {
+    return this._isPropagationStopped;
+};
+
+function stopImmediatePropagation() {
+    this._isImmediatePropagationStopped = true;
+    this._isPropagationStopped = true;
+    if (this._stopImmediatePropagation) {
+        this._stopImmediatePropagation();
+    } else {
+        this.cancelBubble = true;
+    }
+};
+
+function isImmediatePropagationStopped() {
+    return this._isImmediatePropagationStopped;
+};
+
+function preventDefault() {
+    this._isDefaultPrevented = true;
+
+    if (this._preventDefault) {
+        this.preventDefault();
+    } else {
+        this.returnValue = false;
+    }
+};
+
+function isDefaultPrevented() {
+    return this._isDefaultPrevented;
+};
+
+const hookPlugins = {};
+export function registerHook(type, hook) {
+    let hooks = hookPlugins[type] = hookPlugins[type] || [];
+    hooks.push(hook);
+}
 
 function eventHooks(nativeEvent) {
+    // Extend nativeEvent
+    nativeEvent._stopPropagation = nativeEvent.stopPropagation;
+    nativeEvent.stopPropagation = stopPropagation;
+    nativeEvent.isPropagationStopped = isPropagationStopped;
 
-    nativeEvent.stopPropagation = function stopPropagation() {
-        isPropagationStopped = true;
+    nativeEvent._stopImmediatePropagation = nativeEvent.stopImmediatePropagation;
+    nativeEvent.stopImmediatePropagation = stopImmediatePropagation;
+    nativeEvent.isImmediatePropagationStopped = isImmediatePropagationStopped;
 
-        let event = nativeEvent;
-        if (event.stopPropagation) {
-            event.stopPropagation();
-        } else {
-            event.cancelBubble = true;
+    nativeEvent._preventDefault = nativeEvent.preventDefault;
+    nativeEvent.preventDefault = preventDefault;
+    nativeEvent.isDefaultPrevented = isDefaultPrevented;
+
+    // Plugins
+    // register other eventHooks elsewhere, which will be called and injected here
+    // registerHook('scroll', nativeEvent => {
+    //     // logic here
+    // });
+    let hooks = hookPlugins[nativeEvent.type];
+    if (hooks) {
+        let len = hookPlugins.length;
+        for(let i = 0; i < len; i++) {
+            hookPlugins[i](nativeEvent);
         }
-    };
-    nativeEvent.isPropagationStopped = function isPropagationStopped() {
-        return isPropagationStopped;
-    };
-    nativeEvent.preventDefault = function preventDefault() {
-        isDefaultPrevented = true;
+    }
 
-        let event = nativeEvent;
-
-        if (!event) {
-            return;
-        }
-
-        if (event.preventDefault) {
-            event.preventDefault();
-        } else {
-            event.returnValue = false;
-        }
-    };
-    nativeEvent.isDefaultPrevented = function isDefaultPrevented() {
-        return isDefaultPrevented;
-    };
     return nativeEvent;
 }
 
