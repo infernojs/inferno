8a64bbec9d47fc13cee045cdc32c725cc1dd3db6
KFlash <kflash123@gmail.com>
2015-11-29 15:45:00 +0800

    typo fixes

--- src/util/util/uuid.js
+++
@@ -1,4 +0,0 @@
-// Generate a unique identifier
-const uuidFunc = (char) => char == 'x' ? Math.random() * 16 | 0 : (Math.random() * 16 | 0 & 0x3 | 0x8).toString(16);
-
-export default () => 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, uuidFunc);
