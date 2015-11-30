6ae58a6c6107403af662863cc6e12dad6c1ddaff
Tom Jacques <tejacques@gmail.com>
2015-11-30 01:10:47 -0500

    setupEvents only needed for rootNodeHandler, don't need to mimic propagation when directly attaching

--- src/events/createEventListener.js
+++ src/events/createEventListener.js
@@ -1,12 +1,10 @@
 import listenersStorage from './listenersStorage';
 import createListenerArguments from './createListenerArguments';
 import InfernoNodeID from './InfernoNodeID';
-import setupEvents from './setupEvents';
 
 export default function createEventListener(type) {
     return e => {
-        const event = setupEvents(e);
-        const target = event.target;
+        const target = e.target;
         const listener = listenersStorage[InfernoNodeID(target)][type];
         const args = listener.originalHandler.length < 1
             ? [e]
