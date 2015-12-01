80ea964f8e5b74449699ab303e8a28d5e24783a6
KFlash <kflash123@gmail.com>
2015-12-01 22:32:37 +0800

    fixed broken events

--- src/events/InfernoNodeID.js
+++ src/events/InfernoNodeID.js
@@ -1,13 +1,6 @@
-import uuid from '../util/uuid';
-/**
- * To ensure no conflicts with other potential Inferno instances on the page
- */
-const nodeID = '_InfernoNodeID' + uuid;
-
-
 const ID_PROP = '__Inferno__id__';
 let counter = 1;
 
-export default function InfernoNodeID(node, get) {
-    return node[nodeID] || (nodeID ? null : node[nodeID] = counter++);
+export default function InfernoNodeID(node, onlyGet) {
+    return node[ID_PROP] || (onlyGet? null : node[ID_PROP] = counter++);
 }
