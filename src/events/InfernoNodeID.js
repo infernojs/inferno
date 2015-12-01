7b49cf3ef865b3c8db72e530332e69f7901c558c
KFlash <kflash123@gmail.com>
2015-12-02 06:44:04 +0800

    changed null to 0

--- src/events/InfernoNodeID.js
+++ src/events/InfernoNodeID.js
@@ -1,6 +1,7 @@
-const ID_PROP = '__Inferno__id__';
+const INFERNO_PROP = '__Inferno__id__';
 let counter = 1;
 
-export default function InfernoNodeID(node, onlyGet) {
-    return node[ID_PROP] || (onlyGet? null : node[ID_PROP] = counter++);
+export default function InfernoNodeID(node, get) {
+    return node[INFERNO_PROP] || 
+	       (get ? 0 : node[INFERNO_PROP] = counter++);
 }
