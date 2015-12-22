cf5adaba6aa6c1025df7d753521bb5d865b96165
KFlash <kflash123@gmail.com>
2015-12-22 14:24:06 +0800

    improved workflow + license.md

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,31 +1,51 @@
-const fs = require('fs')
-const zlib = require('zlib')
-const rollup = require('rollup')
-const uglify = require('uglify-js')
-const babel = require('rollup-plugin-babel')
-const npm = require('rollup-plugin-npm');
-const pack = require('../package.json')
+import * as p from 'path';
+import * as fs from 'fs';
+import zlib from 'zlib';
+import rollup from 'rollup';
+import babel from 'rollup-plugin-babel';
+import npm from 'rollup-plugin-npm';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import uglify from 'uglify-js';
+import pack from '../package.json';
 
-process.env.NODE_ENV = 'production';
-
-const version = process.env.VERSION || pack.version
-
-const banner =
+const copyright =
     '/*!\n' +
-    ' * ' + pack.name + ' v' + version + '\n' +
+    ' * ' + pack.name + ' v' + pack.version + '\n' +
     ' * (c) ' + new Date().getFullYear() + ' ' + pack.author.name + '\n' +
     ' * Released under the ' + pack.license + ' License.\n' +
     ' */'
+	
+function createBundle() {
+    let bundle = rollup.rollup({
+        entry: p.resolve('src/index.js'),
+        plugins: [
+            babel({
+                babelrc: false,
+                presets: [
+                    'es2015-rollup'
+                ],
+                plugins: [
+                    'transform-object-rest-spread',
+                ],
+            }),
+            npm({
+                jsnext: true,
+                skip: [
+                    'react',
+                ],
+            }),
+            commonjs({
+                sourceMap: false,
+            }),
+            replace({
+                'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+            }),
+        ],
+    });
 
-
-function write(dest, code) {
-    return new Promise(function(resolve, reject) {
-        fs.writeFile(dest, code, function(err) {
-            if (err) return reject(err)
-            console.log('\x1b[1m\x1b[34m' + dest + '\x1b[39m\x1b[22m' + ' ' + (code.length / 1024).toFixed(2) + 'kb')
-            resolve()
-        })
-    })
+    // Cast as native Promise.
+    return Promise.resolve(bundle);
 }
 
 function zip() {
@@ -40,56 +60,69 @@ function zip() {
     })
 }
 
-rollup.rollup({
+function write(dest, code) {
+    return new Promise(function(resolve, reject) {
+        fs.writeFile(dest, code, function(err) {
+            if (err) {
+                return reject(err);
+            }
+            console.log('\x1b[1m\x1b[34m' + dest + '\x1b[39m\x1b[22m' + ' ' + (code.length / 1024).toFixed(2) + 'kb')
+            resolve()
+        })
+    })
+}
 
-        entry: 'src/index.js',
-        plugins: [
-            babel({
-                presets: ['es2015-rollup']
-            }),
-            npm({
-                main: true,
-                jsnext: true
-            })
-        ]
-    })
-    .then(function(bundle) {
-        return write('dist/' + pack.name + '.js', bundle.generate({
-            format: 'umd',
-            banner: banner,
-            moduleName: pack.name
-        }).code)
-    })
-    .then(function() {
-        // Standalone Production Build
-        return rollup.rollup({
-                entry: 'src/index.js',
-                plugins: [
-                    babel({
-                        presets: ['es2015-rollup']
-                    }),
-                    npm({
-                        main: true,
-                        jsnext: true
-                    })
-                ]
-            })
-            .then(function(bundle) {
-                const code = bundle.generate({
-                    format: 'umd',
-                    moduleName: pack.name
-                }).code
-                const minified = uglify.minify(code, {
-                      fromString: true,
-                    unused: true,
-                    dead_code: true,
-                    warnings: false,
-                    screw_ie8: true
-                }).code
-                return write('dist/' + pack.name + '.min.js', minified)
-            })
-            .then(zip)
-    })
-    .catch(function(e) {
-        console.log(e)
-    })
+function writeBundle(bundle, {
+    minify = true
+}) {
+    const filename = minify ? pack.name + '.min.js' : pack.name + '.js';
+    const dest = p.resolve(`dist/${filename}`);
+
+    let result = bundle.generate({
+        format: 'umd',
+        moduleName: 'Inferno',
+        banner: copyright,
+        sourceMap: true,
+        sourceMapFile: dest,
+        globals: {
+        },
+    });
+
+    if (minify) {
+        result = uglify.minify(result.code, {
+            fromString: true,
+            inSourceMap: result.map,
+            outSourceMap: `${filename}.map`,
+            warnings: false,
+        });
+
+        result.map = JSON.parse(result.map);
+    } else {
+        result.code += `\n//# sourceMappingURL=${filename}.map`;
+    }
+
+    let {
+        code,
+        map
+    } = result;
+
+    write(dest, code)
+    write(`${dest}.map`, JSON.stringify(map))
+}
+
+// -----------------------------------------------------------------------------
+
+process.on('unhandledRejection', (reason) => {
+    throw reason;
+});
+
+createBundle().then((bundle) => {
+    writeBundle(bundle, {
+        minify: false
+    }); // Development
+    writeBundle(bundle, {
+        minify: true
+    }); // Production
+	
+	zip(); // gZip
+})
