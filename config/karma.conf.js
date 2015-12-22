ece1c9299ada74fa6947d96dc321f12ed2cc8e7d
KFlash <kflash123@gmail.com>
2015-12-22 08:25:08 +0800

    fixed the coverage reporters

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -46,7 +46,7 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['mocha', 'coverage', 'coveralls'],
+        reporters: ['mocha', 'coverage'],
 
         coverageReporter: {
             type: 'lcov', // lcov or lcovonly are required for generating lcov.info files 
