274b3af9537fbc9877629e9d4b8e3ab6b106b9e3
KFlash <kflash123@gmail.com>
2015-12-23 23:01:26 +0800

    splitted tests into 4

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,6 +1,9 @@
 // inlcude the files you want to run in the browser here
 require('./browser/acceptance/no-jsx/dom-components-tests'); // no-jsx components
-require('./browser/acceptance/no-jsx/dom-elements-tests'); // no-jsx dom-elements-tests
+require('./browser/acceptance/no-jsx/dom-elements-tests1'); // no-jsx dom-elements-tests
+require('./browser/acceptance/no-jsx/dom-elements-tests2'); // no-jsx dom-elements-tests
+require('./browser/acceptance/no-jsx/dom-elements-tests3'); // no-jsx dom-elements-tests
+require('./browser/acceptance/no-jsx/dom-elements-tests4'); // no-jsx dom-elements-tests
 require('./browser/acceptance/no-jsx/svg-tests'); // no-jsx svg tests
 require('./browser/acceptance/no-jsx/event-tests'); // no-jsx event tests
 require('./browser/acceptance/jsx/dom-elements-tests'); // jsx dom-elements-tests
