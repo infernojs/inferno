72f69281e6e317509237f508ec1e90cdcad44d98
KFlash <kflash123@gmail.com>
2015-12-26 08:22:28 +0800

    merge 4 shapes into 2

--- src/DOM/createTree.js
+++ src/DOM/createTree.js
@@ -7,8 +7,6 @@ import createRootNodeWithDynamicChild from './shapes/rootNodeWithDynamicChild';
 import createNodeWithDynamicChild from './shapes/nodeWithDynamicChild';
 import createRootNodeWithDynamicSubTreeForChildren from './shapes/rootNodeWithDynamicSubTreeForChildren';
 import createNodeWithDynamicSubTreeForChildren from './shapes/nodeWithDynamicSubTreeForChildren';
-import createRootStaticNode from './shapes/rootStaticNode';
-import createStaticNode from './shapes/staticNode';
 import createRootDynamicNode from './shapes/rootDynamicNode';
 import createDynamicNode from './shapes/dynamicNode';
 import createRootVoidNode from './shapes/rootVoidNode';
@@ -163,9 +161,9 @@ export default function createDOMTree( schema, isRoot, dynamicNodeMap, domNamesp
 		}
 
 		if ( isRoot ) {
-			node = createRootStaticNode( templateNode );
+			node = createRootVoidNode( templateNode );
 		} else {
-			node = createStaticNode( templateNode );
+			node = createVoidNode( templateNode );
 		}
 	} else {
 		if ( dynamicFlags.NODE === true ) {
