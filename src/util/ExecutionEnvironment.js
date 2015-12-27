8a28a5f14f3300b678f388f1966569171aedf6f6
KFlash <kflash123@gmail.com>
2015-12-27 17:52:54 +0800

    solved Nwjs issues

--- src/util/ExecutionEnvironment.js
+++ src/util/ExecutionEnvironment.js
@@ -1,6 +1,9 @@
-const canUseDOM = !!( typeof window !== 'undefined' &&
-	window.document &&
-	window.document.createElement
+const canUseDOM = !!(
+  typeof window !== 'undefined' &&
+  // Nwjs doesn't add document as a global in their node context, but does have it on window.document,
+  // As a workaround, check if document is undefined
+  typeof document !== 'undefined' &&
+  window.document.createElement
 );
 
 /**
