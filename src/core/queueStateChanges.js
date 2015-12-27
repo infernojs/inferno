6a8d043a8a3327304581048ae9c54ba4c01cce12
KFlash <kflash123@gmail.com>
2015-12-27 09:54:53 +0800

    fixed fucked up code

--- src/core/queueStateChanges.js
+++ src/core/queueStateChanges.js
@@ -1,13 +1,11 @@
 import applyState from './applyState';
 
-export default function queueStateChanges( component, newState ) {
-	for ( const stateKey in newState ) {
-		if ( newState.hasOwnProperty( stateKey ) ) {
-			component._pendingState[stateKey] = newState[stateKey];
-		}
-	}
-	if ( component._pendingSetState === false ) {
-		component._pendingSetState = true;
-		applyState( component );
-	}
+export default function queueStateChanges(component, newState) {
+  for (let stateKey in newState) {
+    component._pendingState[stateKey] = newState[stateKey];
+  }
+  if(component._pendingSetState === false) {
+    component._pendingSetState = true;
+    applyState(component);
+  }
 }
