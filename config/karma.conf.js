027cdad7a0f71d0dc0761740a264284a21f95e79
KFlash <kflash123@gmail.com>
2015-12-29 04:59:32 +0800

    Update karma.conf.js

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -49,7 +49,7 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['mocha', 'coverage'],
+        reporters: ['mocha' /*, 'coverage'*/],
 
         coverageReporter: {
             reporters: [{
