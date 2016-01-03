f0d9462754eb66a4a254e9fdd18a5fb646a25079
KFlash <kflash123@gmail.com>
2016-01-03 15:10:36 +0800

    more tests

---
+++ src/DOM/__tests__/creation.spec.browser.js
@@ -0,0 +1,259 @@
+import template from '../index';
+import { render, renderToString } from '../rendering';
+import createTemplate from '../../core/createTemplate';
+import createDOMTree from '../createTree';
+import { addTreeConstructor } from '../../core/createTemplate';
+
+addTreeConstructor( 'dom', createDOMTree );
+
+describe( 'Creation - (non-JSX)', () => {
+
+	let container;
+
+	beforeEach(function() {
+		container = document.createElement('div');
+	});
+
+	afterEach(function() {
+		container.innerHTML = '';
+	});
+
+	[{
+		description: 'should render div with span child',
+		template: () => {
+			return {
+				tag: 'div',
+				children: {
+					tag: 'span'
+				}
+			}
+		},
+		tagName: 'div',
+		children: 1,
+		textContent: ''
+	}, {
+		description: 'should render span with span child',
+		template: () => {
+			return {
+				tag: 'span',
+				children: {
+					tag: 'span'
+				}
+			}
+		},
+		tagName: 'span',
+		children: 1,
+		textContent: ''
+	}, {
+		description: 'should render div with two span children',
+		template: () => {
+			return {
+				tag: 'div',
+				children: [{
+					tag: 'span'
+				}, {
+					tag: 'span'
+				}]
+			}
+		},
+		tagName: 'div',
+		children: 2,
+		textContent: ''
+	}, {
+		description: 'should render div with three span children and unset middle child',
+		template: () => {
+			return {
+				tag: 'div',
+				children: [{
+					tag: 'span'
+				},
+					null, {
+						tag: 'span'
+					}
+				]
+			}
+		},
+		tagName: 'div',
+		children: 2,
+		textContent: ''
+	}, {
+		description: 'should render div with three span children and unset first, and middle child',
+		template: () => {
+			return {
+				tag: 'div',
+				children: [
+					null,
+					null, {
+						tag: 'span'
+					}
+				]
+			}
+		},
+		tagName: 'div',
+		children: 1,
+		textContent: ''
+	}, {
+		description: 'should render div with one textNode and a span children',
+		template: () => {
+			return {
+				tag: 'div',
+				children: [
+					'Hello!',
+					null, {
+						tag: 'span'
+					}
+				]
+			}
+		},
+		tagName: 'div',
+		children: 2,
+		textContent: 'Hello!'
+	}, {
+		description: 'should render div with two textNodes and a span children',
+		template: () => {
+			return {
+				tag: 'div',
+				children: [
+					'Hello, ',
+					'World!', {
+						tag: 'span'
+					}
+				]
+			}
+		},
+		tagName: 'div',
+		children: 3,
+		textContent: 'Hello, World!'
+	}, {
+		description: 'should render div with two textNodes and a two span children',
+		template: () => {
+			return {
+				tag: 'div',
+				children: [
+					'Hello, ', {
+						tag: 'span'
+					},
+					'World!', {
+						tag: 'span'
+					}
+				]
+			}
+		},
+		tagName: 'div',
+		children: 4,
+		textContent: 'Hello, World!'
+	}, {
+		description: 'should render div with two textNodes and one span children, and span with textNode',
+		template: () => {
+			return {
+				tag: 'div',
+				children: [
+					'Hello', {
+						tag: 'span'
+					},
+					', ', {
+						tag: 'span',
+						children: 'World!'
+					}
+				]
+			}
+		},
+		tagName: 'div',
+		children: 4,
+		textContent: 'Hello, World!'
+	}, {
+		description: 'should render div with tree null values in an array for children',
+		template: () => {
+			return {
+				tag: 'div',
+				children: [
+					null,
+					null,
+					null
+				]
+			}
+		},
+		tagName: 'div',
+		children: 0,
+		textContent: ''
+	}, {
+		description: 'should render div with b child, and tree null values in an array for children',
+		template: () => {
+			return {
+				tag: 'div',
+				children: {
+					tag: 'b',
+					children:
+						[
+							null,
+							null,
+							null
+						]
+				}
+			}
+		},
+		tagName: 'div',
+		children: 1,
+		textContent: ''
+	}, {
+		description: 'should render div with b child, and number and two null values in an array for children',
+		template: () => {
+			return {
+				tag: 'div',
+				children: {
+					tag: 'b',
+					children:
+						[
+							null,
+							123,
+							null
+						]
+				}
+			}
+		},
+		tagName: 'div',
+		children: 1,
+		textContent: '123'
+	}, {
+		description: 'should render empty div',
+		template: () => {
+			return {
+				tag: 'div'
+			};
+		},
+		tagName: 'div',
+		children: 0,
+		textContent: ''
+	}, {
+		description: 'should render empty span',
+		template: () => {
+			return {
+				tag: 'span'
+			};
+		},
+		tagName: 'span',
+		children: 0,
+		textContent: ''
+	}].forEach((test) => {
+
+		it(test.description, () => {
+
+			render(createTemplate(test.template)(), container);
+			expect(container.firstChild.nodeType).to.equal(1);
+			expect(container.firstChild.tagName.toLowerCase()).to.equal(test.tagName);
+			expect(container.firstChild.childNodes.length).to.equal(test.children);
+			expect(container.firstChild.textContent).to.equal(test.textContent);
+
+			render(createTemplate(test.template)(), container);
+			expect(container.firstChild.nodeType).to.equal(1);
+			expect(container.firstChild.tagName.toLowerCase()).to.equal(test.tagName);
+			expect(container.firstChild.childNodes.length).to.equal(test.children);
+
+			render(createTemplate(test.template)(), container);
+			expect(container.firstChild.nodeType).to.equal(1);
+			expect(container.firstChild.tagName.toLowerCase()).to.equal(test.tagName);
+			expect(container.firstChild.childNodes.length).to.equal(test.children);
+
+		});
+	});
+});
--- src/DOM/__tests__/styles.spec.browser.js
+++ src/DOM/__tests__/styles.spec.browser.js
@@ -2,6 +2,7 @@ import createDOMTree from '../createTree';
 import { render } from '../rendering';
 import createTemplate from '../../core/createTemplate';
 import { addTreeConstructor } from '../../core/createTemplate';
+import { shortCuts, cssToJSName } from '../../util/styleAccessor';
 
 addTreeConstructor( 'dom', createDOMTree );
 
@@ -249,4 +250,76 @@ describe( 'CSS style properties', () => {
 			});
 		});
 	});
+
+	describe( 'Shorthand CSS Styles', () => {
+		Object.keys( shortCuts ).forEach( shortCut => {
+			let stylePropName = cssToJSName( shortCut );
+			let shorthands = shortCuts[shortCut];
+			let mustBeString = ( /style/ig ).test( shortCut );
+
+			if ( shorthands.length ) {
+				let val = mustBeString ? 'dotted' : 1;
+				let style = { [stylePropName]: val };
+				let comparator = mustBeString ? val : val + 'px';
+
+				describe( `Set ${shortCut} CSS properties from shorthand: ${JSON.stringify( style ) }`, () => {
+
+					beforeEach(() => {
+						let template = createTemplate(() => {
+							return {
+								tag: 'div',
+								attrs: {
+									style: style
+								}
+							}
+						});
+						render(template(), container);
+					});
+
+					shorthands.forEach( cssProperty => {
+						it( `should set ${cssProperty} to ${style[stylePropName]}px`, () => {
+							expect( container.firstChild.style[cssProperty] ).to.equal( comparator );
+						} );
+					} );
+				} );
+			}
+
+			if ( shorthands.length ) {
+				[{
+					numbers: [ 1, 2 ],
+					strings: [ 'dotted', 'solid' ]
+				}, {
+					numbers: [ 1, 2, 3, 4 ],
+					strings: [ 'dotted', 'solid', 'dashed', 'double' ]
+				}].forEach( vals => {
+
+					let values = mustBeString ? vals.strings : vals.numbers.map( x => x + 'px' );
+					let val = values.join( ' ' );
+					let style = { [stylePropName]: val };
+
+					describe( `Set ${shortCut} CSS properties from shorthand: ${JSON.stringify( style ) }`, () => {
+
+						beforeEach(() => {
+							let template = createTemplate(() => {
+								return {
+									tag: 'div',
+									attrs: {
+										style: style
+									}
+								}
+							});
+							render(template(), container);
+						});
+						shorthands.forEach( ( cssProperty, index ) => {
+							let comparator = values[index % values.length];
+
+							it( `should set ${cssProperty} to ${comparator}`, () => {
+								expect( container.firstChild.style[cssProperty] ).to.equal( comparator );
+							} );
+						} );
+					} );
+				} );
+			}
+		} );
+	} );
 });
