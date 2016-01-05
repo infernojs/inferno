6c8cebf374f1a4bd0b25b3a0cbab7ef5e2a9850e
KFlash <kflash123@gmail.com>
2016-01-06 06:00:29 +0800

    removed duplicate code

---
+++ src/shared/addShapeChildren.js
@@ -0,0 +1,32 @@
+import isVoid from '../util/isVoid';
+import isArray from '../util/isArray';
+
+export default function addShapeChildren(domNode, subTreeForChildren, item, treeLifecycle, context ) {
+
+	if ( !isVoid( subTreeForChildren ) ) {
+		if ( isArray( subTreeForChildren ) ) {
+			for ( let i = 0; i < subTreeForChildren.length; i++ ) {
+				const subTree = subTreeForChildren[i];
+				const childNode = subTree.create( item, treeLifecycle, context );
+
+				domNode.appendChild( childNode );
+			}
+		} else if ( typeof subTreeForChildren === 'object' ) {
+			domNode.appendChild( subTreeForChildren.create( item, treeLifecycle, context ) );
+		}
+	}
+}
+/*if ( !isVoid( subTreeForChildren ) ) {
+	if ( isArray( subTreeForChildren ) ) {
+		for ( let i = 0; i < subTreeForChildren.length; i++ ) {
+			const subTree = subTreeForChildren[i];
+
+			domNode.appendChild( subTree.create( item, treeLifecycle, context ) );
+		}
+	} else if ( typeof subTreeForChildren === 'object' ) {
+		domNode.appendChild( subTreeForChildren.create( item, treeLifecycle, context ) );
+	}
+}
+ */
+
+
