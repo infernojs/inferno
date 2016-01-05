6955949e332f925cb406f40d7824a3736f18d726
KFlash <kflash123@gmail.com>
2016-01-05 11:26:16 +0800

    improved storage()

--- src/util/storage.js
+++ src/util/storage.js
@@ -1,16 +1,3 @@
-function doesItExist(value) {
-
-	let i;
-
-	if ( value != value || value === 0 ) {
-
-		for ( i = this.length; i-- && !is( this[ i ], value ); );
-	} else {
-		i = [].indexOf.call( this, value );
-	}
-	return i;
-}
-
 function Storage( iterable ){
 	let _items = [];
 	let _keys = [];
@@ -20,14 +7,15 @@ function Storage( iterable ){
 
 		get: {
 			value( key ) {
-				const index = doesItExist.call(_keys, key );
-				return index > -1 ? _values[index] : undefined;
+				const index = [].indexOf.call(_keys, key );
+				return  _values[index] || undefined;
 			}
 		},
 		set: {
 			value( key, value ) {
 				// check if key exists and overwrite
-				const index = doesItExist.call(_keys, key );
+
+				const index = [].indexOf.call( _keys, key );
 				if ( index > -1 ) {
 					_items[index][1] = value;
 					_values[index] = value;
