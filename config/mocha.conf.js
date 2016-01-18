2bda3ca74c3f946bfafcb5917737f39bf8e4c583
KFlash <kflash123@gmail.com>
2016-01-18 21:02:18 +0800

    environment refactoring

--- config/mocha.conf.js
+++ config/mocha.conf.js
@@ -1,7 +1,4 @@
 /* eslint-disable */
-
-process.env.NODE_ENV = 'development';
-
 global.babel = require('babel-core/register');
 global.expect = require('chai').expect;
 global.sinon = require('sinon');
