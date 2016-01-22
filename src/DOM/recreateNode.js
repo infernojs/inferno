b8e88355d656d167a402d800670bbde338d94b30
KFlash <kflash123@gmail.com>
2016-01-22 22:07:15 +0800

    halfway done

--- src/DOM/recreateNode.js
+++ src/DOM/recreateNode.js
@@ -1,6 +1,14 @@
 export default function recreateNode(lastDomNode, nextItem, node, treeLifecycle, context) {
 	const domNode = node.create(nextItem, treeLifecycle, context);
 
-	lastDomNode.parentNode.replaceChild(domNode, lastDomNode);
+	if (lastDomNode.parentNode) {
+		lastDomNode.parentNode.replaceChild(domNode, lastDomNode);
+	} else {
+		const newNode = lastDomNode.tree.dom.create(nextItem, treeLifecycle, context);
+
+		// TODO! Add missing code
+
+	}
+
 	// TODO recycle old node
 }
