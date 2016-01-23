d2b4cb66ad94ec253afeed6925d5329cc0ec9cfa
Tom Jacques <tejacques@gmail.com>
2016-01-22 22:58:57 -0500

    Treat Inferno as an external module for bundlers, and use standard import syntax -- works with AMD now

--- packages/inferno-dom/src/index.js
+++ packages/inferno-dom/src/index.js
@@ -1,31 +1,13 @@
 import { render } from '../../../src/DOM/rendering';
 import createRef from '../../../src/DOM/createRef';
 import createDOMTree from '../../../src/DOM/createTree';
+import Inferno from 'inferno';
 
-const GLOBAL = typeof global !== 'undefined' ? global : (typeof window !== 'undefined' ? window : null);
-
-// browser
-if (GLOBAL && GLOBAL.Inferno) {
-	GLOBAL.Inferno.addTreeConstructor('dom', createDOMTree);
-// nodeJS
-// TODO! Find a better way to detect if we are running in Node, and test if this actually works!!!
-} else if ( GLOBAL && !GLOBAL.Inferno ) {
-	let Inferno;
-
-	// TODO! Avoid try / catch
-	try {
-		Inferno = require('inferno');
-	} catch (e) {
-		Inferno = null;
-		// TODO Should we throw a warning and inform that the Inferno package is not installed?
-	}
-
-	if (Inferno != null) {
-		if (typeof Inferno.addTreeConstructor !== 'function') {
-			throw ('Your package is out-of-date! Upgrade to latest Inferno in order to use the InfernoDOM package.');
-		} else {
-			Inferno.addTreeConstructor('dom', createDOMTree);
-		}
+if (Inferno) {
+	if (typeof Inferno.addTreeConstructor !== 'function') {
+		throw ('Your package is out-of-date! Upgrade to latest Inferno in order to use the InfernoDOM package.');
+	} else {
+		Inferno.addTreeConstructor('dom', createDOMTree);
 	}
 }
 
