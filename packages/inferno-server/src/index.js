70420c7e3e8fbe648e43db1b9f74e00ec672622b
KFlash <kflash123@gmail.com>
2016-01-24 08:08:27 +0800

    fixed packages

--- packages/inferno-server/src/index.js
+++ packages/inferno-server/src/index.js
@@ -1,33 +1,16 @@
 import renderToString from '../../../src/server/renderToString';
 import createHTMLTree from '../../../src/server/createTree';
+import Inferno from 'inferno';
 
-const GLOBAL = typeof global !== 'undefined' ? global : (typeof window !== 'undefined' ? window : null);
-
-// browser
-if (GLOBAL && GLOBAL.Inferno) {
-	GLOBAL.Inferno.addTreeConstructor('html', createHTMLTree);
-// nodeJS
-// TODO! Find a better way to detect if we are running in Node, and test if this actually works!!!
-} else if (GLOBAL && !GLOBAL.Inferno) {
-	let Inferno;
-
-	// TODO! Avoid try / catch
-	try {
-		Inferno = require('inferno');
-	} catch (e) {
-		Inferno = null;
-		// TODO Should we throw a warning and inform that the Inferno package is not installed?
-	}
-
-	if (Inferno != null) {
-		if (typeof Inferno.addTreeConstructor !== 'function') {
-			throw ('Your package is out-of-date! Upgrade to latest Inferno in order to use the InfernoServer package.');
-		} else {
-			Inferno.addTreeConstructor('html', createHTMLTree);
-		}
+if (Inferno) {
+	if (typeof Inferno.addTreeConstructor !== 'function') {
+		throw ('Your package is out-of-date! Upgrade to latest Inferno in order to use the InfernoDOM package.');
+	} else {
+		Inferno.addTreeConstructor('html', createDOMTree);
 	}
 }
 
 export default {
 	renderToString
 };
+
--- packages/inferno-test-utils/src/index.js
+++ packages/inferno-test-utils/src/index.js
@@ -3,31 +3,13 @@ import deepRender from '../../../src/testUtils/deepRender';
 import createTestTree from '../../../src/testUtils/createTree';
 import renderIntoDocument from '../../../src/testUtils/renderIntoDocument';
 import Simulate from '../../../src/testUtils/Simulate';
+import Inferno from 'inferno';
 
-const GLOBAL = typeof global !== 'undefined' ? global : (typeof window !== 'undefined' ? window : null);
-
-// browser
-if (GLOBAL && GLOBAL.Inferno) {
-	GLOBAL.Inferno.addTreeConstructor('test', createTestTree);
-// nodeJS
-// TODO! Find a better way to detect if we are running in Node, and test if this actually works!!!
-} else if (GLOBAL && !GLOBAL.Inferno) {
-	let Inferno;
-
-	// TODO! Avoid try / catch
-	try {
-		Inferno = require('inferno');
-	} catch (e) {
-		Inferno = null;
-		// TODO Should we throw a warning and inform that the Inferno package is not installed?
-	}
-
-	if (Inferno != null) {
-		if (typeof Inferno.addTreeConstructor !== 'function') {
-			throw ('Your package is out-of-date! Upgrade to latest Inferno in order to use the InfernoTestUtils package.');
-		} else {
-			Inferno.addTreeConstructor('test', createTestTree);
-		}
+if (Inferno) {
+	if (typeof Inferno.addTreeConstructor !== 'function') {
+		throw ('Your package is out-of-date! Upgrade to latest Inferno in order to use the InfernoDOM package.');
+	} else {
+		Inferno.addTreeConstructor('test', createDOMTree);
 	}
 }
 
@@ -37,3 +19,4 @@ export default {
 	renderIntoDocument
 	//Simulate
 };
+
