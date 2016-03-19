1005bbe82a670e09d524f73e559b06608496b45e
Dominic Gannaway <dg@domgan.com>
2016-03-19 09:48:08 +0000

    added test case per #136

--- src/DOM/__tests__/events.spec.browser.js
+++ src/DOM/__tests__/events.spec.browser.js
@@ -8,9 +8,11 @@ describe('Basic event tests', () => {
 
     beforeEach(() => {
         container = document.createElement('div');
+	    document.body.appendChild(container);
     });
 
     afterEach(() => {
+		document.body.removeChild(container);
         container.innerHTML = '';
     });
 
@@ -39,8 +41,6 @@ describe('Basic event tests', () => {
 
         render(template(test), container);
 
-        document.body.appendChild(container);
-
         let divs = Array.prototype.slice.call(container.querySelectorAll('div'));
         divs.forEach(div => div.click());
         expect(calledFirstTest).to.equal(true);
@@ -68,4 +68,46 @@ describe('Basic event tests', () => {
         expect(calledSecondTest).to.equal(false);
         done();
     });
+
+	it('should update events', () => {
+		var data = {
+			count: 0
+		};
+
+		function onClick(d) {
+			return function (e) {
+				data = { count: d.count + 1 };
+
+				renderIt();
+			};
+		}
+		function App(d) {
+			return {
+				tag: "button",
+				events: {
+					click: onClick(d)
+				},
+				children: ['Count ', d.count],
+				dom: null
+			};
+		}
+
+		function renderIt() {
+			render(App(data), container);
+		}
+
+		renderIt();
+		const buttons = Array.prototype.slice.call(container.querySelectorAll('button'));
+
+		expect(container.firstChild.innerHTML).to.equal('Count 0');
+		expect(data.count).to.equal(0);
+		buttons.forEach(button => button.click());
+		expect(container.firstChild.innerHTML).to.equal('Count 1');
+		expect(data.count).to.equal(1);
+		buttons.forEach(button => button.click());
+		expect(container.firstChild.innerHTML).to.equal('Count 2');
+		expect(data.count).to.equal(2);
+	});
 });
+
+
