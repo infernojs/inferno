79f10d8df8046b78816834f543942235ffa85339
KFlash <kflash123@gmail.com>
2016-04-16 06:59:43 +0800

    Update karma.conf.js

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function (config) {
 		browsers: ['Chrome'],
 		customLaunchers: {
 			ChromeForTravisCI: {
-				base: 'ChromeCanary',
+				base: 'Chrome',
 				flags: ['--no-sandbox']
 			}
 		},
