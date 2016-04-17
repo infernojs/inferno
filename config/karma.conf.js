0c8b86a1d63fe87fef71b32e77a1b6a0ebb0a1a4
KFlash <YOUR EMAIL ADDRESS>
2016-04-17 22:38:12 +0800

    phantomjs

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
 		// - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
 		// - PhantomJS
 		// - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-		browsers: ['Chrome'],
+		browsers: ['PhantomJS'],
 		// list of files to exclude
 		exclude: [],
 		preprocessors: {
