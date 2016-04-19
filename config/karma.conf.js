17f1723c9c7400e092ab128d804f64a413db50aa
KFlash <kflash123@gmail.com>
2016-04-20 06:26:59 +0800

    Update karma.conf.js

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
 		// - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
 		// - PhantomJS
 		// - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-		browsers: ['PhantomJS'],
+		browsers: ['Chrome'],
 		customLaunchers: {
 			Chrome_travis_ci: {
 				base: 'Chrome',
