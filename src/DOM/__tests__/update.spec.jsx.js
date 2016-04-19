5960c82c7127628554986216427ed6d18f6e3dfd
Sampo Kivistö <sampo.kivisto@live.fi>
2016-04-19 20:30:46 +0300

    remove old docs folder, add new failing test to reproduce issue with component updates

---
+++ src/DOM/__tests__/update.spec.jsx.js
@@ -0,0 +1,70 @@
+import { render } from './../rendering';
+import { createBlueprint } from './../../core/createBlueprint';
+import Component from './../../component/index';
+
+const Inferno = {
+    createBlueprint
+};
+
+describe('Statefull Component updates', () => {
+
+    let container;
+
+    beforeEach(() => {
+        container = document.createElement('div');
+    });
+
+    afterEach(() => {
+        container.innerHTML = '';
+    });
+
+    it('Should forget old updates', (done) => {
+        let updatesAfromOutside;
+
+        class A extends Component {
+            constructor(props) {
+                super(props);
+
+                this.state = {
+                    stuff: true
+                };
+
+                updatesAfromOutside = this.updateMe.bind(this);
+            }
+
+            updateMe() {
+                this.setState({
+                    stuff: false
+                })
+            }
+
+            render() {
+                return <div>A Component A</div>;
+            }
+        }
+
+        class B extends Component {
+            constructor(props) {
+                super(props);
+            }
+
+            render() {
+                return <div>B Component B</div>;
+            }
+        }
+
+        // Render A
+        render(<A />, container);
+        expect(container.innerHTML).to.equal('<div>A Component A</div>');
+        // Render B
+        render(<B />, container);
+        expect(container.innerHTML).to.equal('<div>B Component B</div>');
+
+        // delayed update triggers for A
+        updatesAfromOutside();
+        expect(container.innerHTML).to.equal('<div>B Component B</div>');
+
+
+        done();
+    });
+});
