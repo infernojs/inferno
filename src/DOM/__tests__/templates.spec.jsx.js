4eee41ab3ea8f9e108c3c67c05ee85db251f3614
Sampo Kivistö <sampo.kivisto@live.fi>
2016-08-15 12:20:46 +0300

    Added test case to reproduce exception: replaceChild of undefined

---
+++ src/DOM/__tests__/templates.spec.jsx.js
@@ -0,0 +1,104 @@
+import { render } from './../rendering';
+import Component from './../../component/es2015';
+import { createVTemplate, createVElement, createVComponent } from './../../core/shapes';
+import { createTemplateReducers } from './../../DOM/templates';
+import innerHTML from "../../../tools/innerHTML";
+
+const Inferno = {
+	createVTemplate,
+	createVElement,
+	createVComponent
+};
+const InfernoDOM = {
+	createTemplateReducers
+};
+
+describe('Templates', () => {
+	let container;
+
+	beforeEach(() => {
+		container = document.createElement('div');
+	});
+
+	describe('Should have parentDOM defined #1', () => {
+		class A extends Component {
+			render() {
+				return <div>A</div>
+			}
+		}
+
+		class B extends Component {
+			render() {
+				return <span>B</span>
+			}
+		}
+
+		class Counter extends Component {
+			constructor(props) {
+				super(props);
+				this.state = {
+					bool: false
+				};
+				this.btnCount = this.btnCount.bind(this);
+			}
+			btnCount(){
+				this.setState({
+					bool: !this.state.bool
+				});
+			}
+			render(){
+				return (
+					<div class="my-component">
+						<h1>{ this.props.car } { this.state.bool ? <A /> : <B /> }</h1>
+						<button type="button" onClick={ this.btnCount }>btn</button>
+					</div>
+				);
+			}
+		}
+
+		class Wrapper extends Component {
+			constructor(props) {
+				super(props);
+			}
+			render() {
+				return (
+					<div>
+						{ [ 'Saab', 'Volvo', 'BMW' ].map(function (c) {
+							return (<Counter car={ c } />);
+						}) }
+					</div>
+				);
+			}
+		}
+
+		it('Initial render (creation)', () => {
+			render(<Wrapper/>, container);
+
+			expect(
+				container.innerHTML
+			).to.equal(
+				innerHTML('<div><div class="my-component"><h1>Saab <span>B</span></h1><button type="button">btn</button></div><div class="my-component"><h1>Volvo <span>B</span></h1><button type="button">btn</button></div><div class="my-component"><h1>BMW <span>B</span></h1><button type="button">btn</button></div></div>')
+			);
+
+			render(null, container);
+		});
+
+		it('Second render (update)', (done) => {
+			render(<Wrapper/>, container);
+			const buttons = Array.prototype.slice.call(container.querySelectorAll('button'));
+			buttons.forEach(button => button.click());
+
+			// requestAnimationFrame is needed here because
+			// setState fires after a requestAnimationFrame
+			requestAnimationFrame(() => {
+				expect(
+					container.innerHTML
+				).to.equal(
+					innerHTML('<div><div class="my-component"><h1>Saab <div>A</div></h1><button type="button">btn</button></div><div class="my-component"><h1>Volvo <div>A</div></h1><button type="button">btn</button></div><div class="my-component"><h1>BMW <div>A</div></h1><button type="button">btn</button></div></div>')
+				);
+				render(null, container);
+				done();
+			});
+		});
+	});
+});
