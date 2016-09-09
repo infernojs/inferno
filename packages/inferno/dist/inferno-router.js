3bdd4ce1c7fc8e669a4ffe68635c3c14563c6732
Dominic Gannaway <dg@domgan.com>
2016-09-09 12:00:57 +0100

    updated version

--- packages/inferno/dist/inferno-router.js
+++ packages/inferno/dist/inferno-router.js
@@ -1,5 +1,5 @@
 /*!
- * inferno-router v1.0.0-alpha1
+ * inferno-router v1.0.0-alpha2
  * (c) 2016 Dominic Gannaway
  * Released under the MIT License.
  */
--- packages/inferno/dist/inferno.js
+++ packages/inferno/dist/inferno.js
@@ -1,5 +1,5 @@
 /*!
- * inferno v1.0.0-alpha1
+ * inferno v1.0.0-alpha2
  * (c) 2016 Dominic Gannaway
  * Released under the MIT License.
  */
