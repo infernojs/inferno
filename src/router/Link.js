0da33d5517f5e79728e3d170282401ad6fcdfb0b
Vitor Buzinaro <funny.hc@gmail.com>
2016-09-10 05:11:23 -0300

    fix Link to not preventDefault when middle clicked
    The mouse middle button click shouldn't prevent the default behavior (open in a new Tab).
    Added a verification to only make the client routing if the left button is clicked.

--- src/router/Link.js
+++ src/router/Link.js
@@ -22,6 +22,10 @@ export default function Link(props, { hashbang, history }) {
 	if (!hashbang) {
 		element.events({
 			onclick: function navigate(e) {
+				if (e.button !== 1) {
+					return;
+				}
+				
 				e.preventDefault();
 				const target = e.target;
 				window.history.pushState(null, target.textContent, to);
