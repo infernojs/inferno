c7ce5c9b6dfb8fba016727e92ad918e05e7486c4
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-09-19 23:02:47 +0200

    Use tabs for indentation

--- src/DOM/lifecycle.ts
+++ src/DOM/lifecycle.ts
@@ -1,15 +1,15 @@
 export default class Lifecycle {
-  private _listeners: Array<Function>;
+	private _listeners: Array<Function>;
 
-  constructor() {
-    this._listeners = [];
-  }
-  addListener(callback) {
-    this._listeners.push(callback);
-  }
-  trigger() {
-    for (let i = 0; i < this._listeners.length; i++) {
-      this._listeners[i]();
-    }
-  }
+	constructor() {
+		this._listeners = [];
+	}
+	addListener(callback) {
+		this._listeners.push(callback);
+	}
+	trigger() {
+		for (let i = 0; i < this._listeners.length; i++) {
+			this._listeners[i]();
+		}
+	}
 }
