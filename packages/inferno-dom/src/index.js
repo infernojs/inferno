ca5a4c852eccc43e77b958c47fa7efcf4bc579b6
Dominic Gannaway <dg@domgan.com>
2016-09-27 22:35:16 +0100

    fixes tiger example and adds a DEV TOOLS hook

--- packages/inferno-dom/src/index.js
+++ packages/inferno-dom/src/index.js
@@ -1,6 +1,8 @@
-import { render, findDOMNode, createRenderer } from '../../../src/DOM/rendering';
+import { render, findDOMNode, createRenderer, roots } from '../../../src/DOM/rendering';
 import createStaticVElementClone from '../../../src/factories/createStaticVElementClone';
 import { disableRecycling } from '../../../src/DOM/recycling';
+
+window.__INFERNO_DEVTOOLS_DOM_ROOTS__ = roots;
 
 export default {
 	render,
