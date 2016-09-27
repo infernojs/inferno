b7c49aac0beb0e39d19f512c27c0e3451dabaab8
Ryan Megidov <Ryan Megidov>
2016-09-27 15:46:39 +0200

    Make peace with tslint #2

--- src/mobx/modules.d.ts
+++ src/mobx/modules.d.ts
@@ -21,3 +21,11 @@ declare module 'inferno-create-element' {
 	function createElement(component: any, props: any, children: any): any;
 	export = createElement;
 }
+
+declare module 'mobx' {
+	export function isObservable(value: any, property?: string): boolean;
+	export class Reaction {
+		constructor(name?: string, onInvalidate?: () => void);
+	};
+	export const extras: any;
+}
