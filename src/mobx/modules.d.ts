a092e00f77d2c3b6535f4201ac46f969d8c68385
Ryan Megidov <Ryan Megidov>
2016-09-28 00:48:54 +0200

    Make peace with tslint #6

--- src/mobx/modules.d.ts
+++ src/mobx/modules.d.ts
@@ -9,16 +9,16 @@ declare module 'inferno-component' {
 }
 
 declare module 'inferno-dom' {
-	export function findDOMNode(node: any): any;
+	export function findDOMNode(node: any): any
 }
 
 declare module 'inferno-create-class' {
-	function createClass(component: any): any;
+	function createClass(component: any): any
 	export = createClass;
 }
 
 declare module 'inferno-create-element' {
-	function createElement(component: any, props: any, children: any): any;
+	function createElement(component: any, props: any, children: any): any
 	export = createElement;
 }
 
