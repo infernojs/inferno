2feac301c788c91bd7ff71fc012f481f2c8119aa
Ryan Megidov <Ryan Megidov>
2016-09-28 18:13:57 +0200

    Make sure we're not on node.js before assigning to window

--- packages/inferno-dom/src/index.js
+++ packages/inferno-dom/src/index.js
@@ -2,7 +2,9 @@ import { render, findDOMNode, createRenderer, roots } from '../../../src/DOM/ren
 import createStaticVElementClone from '../../../src/factories/createStaticVElementClone';
 import { disableRecycling } from '../../../src/DOM/recycling';
 
-window.__INFERNO_DEVTOOLS_DOM_ROOTS__ = roots;
+if (typeof window !== 'undefined' && window.document) {
+	window.__INFERNO_DEVTOOLS_DOM_ROOTS__ = roots;
+}
 
 export default {
 	render,
