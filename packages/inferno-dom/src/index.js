97332b83360158149647bd3a586ab0602bf2d093
Dominic Gannaway <dg@domgan.com>
2016-10-03 20:48:32 +0100

    WIP dev tools update

--- packages/inferno-dom/src/index.js
+++ packages/inferno-dom/src/index.js
@@ -3,7 +3,12 @@ import createStaticVElementClone from '../../../src/factories/createStaticVEleme
 import { disableRecycling } from '../../../src/DOM/recycling';
 
 if (typeof window !== 'undefined' && window.document) {
+	// WIP – no idea what I'm doing really?
 	window.__INFERNO_DEVTOOLS_DOM_ROOTS__ = roots;
+	
+	window.addEventListener('inferno.devtools.init', function (event) {
+		debugger;
+	});
 }
 
 export default {
