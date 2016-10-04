6bf2580236aa1062f834d31ef8ae70a2282bde29
Dominic Gannaway <dg@domgan.com>
2016-10-05 00:43:18 +0100

    updated devtools

--- src/DOM/devtools.ts
+++ src/DOM/devtools.ts
@@ -1,41 +1,23 @@
 import { roots } from './rendering';
-	
 
-	// var backgroundPageConnection = chrome.runtime.connect({
-	// 	name: "panel"
-	// });
-	
-	// window.addEventListener('message', function (event) {
-	// 	var backgroundPageConnection = chrome.runtime.connect({
-	// 		name: "panel"
-	// 	});
-
-	// 	backgroundPageConnection.postMessage({
-	// 		name: 'init',
-	// 		tabId: chrome.devtools.inspectedWindow.tabId
-	// 	});
-	// });
-
+function sendToDevTools(global, data) {
+	var event = new CustomEvent('inferno.client.message', {
+		detail: JSON.stringify(data)
+	});
+	global.dispatchEvent(event);
+}
 
 export function initDevToolsHooks(global) {
 	global.__INFERNO_DEVTOOLS_GLOBAL_HOOK__ = true;
 
-	// we now need to wait for a message?
-	global.addEventListener('message', function(event) {
-		console.log(event)
-		// Only accept messages from the same frame
-		if (event.source !== window) {
-			return;
+	global.addEventListener('inferno.devtools.message', function (message) {
+		const detail = JSON.parse(message.detail);
+		const type = detail.type;
+
+		switch (type) {
+			case 'init':
+				sendToDevTools(global, { type: 'roots', data: Array.from(roots.values()) });
+				break;
 		}
-
-		var message = event.data;
-
-		// Only accept messages that we know are ours
-		if (typeof message !== 'object' || message === null ||
-			message.source !== 'my-devtools-extension') {
-			return;
-		}
-
-		global.chrome.runtime.sendMessage('my-devtools-extension', message);
 	});
 }
