ba7c6e1eae75f440fcaf567915f0c04089d1ca73
Sharon Rolel <sharonrolel@gmail.com>
2016-10-11 08:02:23 +0300

    reverse tranpilation order - buble can't ignore jsx

--- config/compiler.js
+++ config/compiler.js
@@ -7,7 +7,9 @@ var merge = require('merge-source-map');
 
 var nodeVersion = /(?:0\.)?\d+/.exec(process.version)[0];
 var nodeModulesPattern = path.sep === '/' ? /\/node_modules\// : /\\node_modules\\/;
-var options = {
+
+var bubleOptions = {
+	objectAssign: 'Object.assign',
 	target: {
 		node: nodeVersion
 	},
@@ -17,53 +19,62 @@ var options = {
 	}
 };
 
-var old = require.extensions['.ts'];
-if (!old) {
-	throw new Error('ts-node/register or equivalent compiler must be used before this compiler.')
-}
-
-require.extensions['.ts'] = function (m, filename) {
-	
-	if ( nodeModulesPattern.test( filename ) ) return old( m, filename );
-	var _compile = m._compile;
-	m._compile = function (code, fileName) { 
-		var compiled;
-		try {
-			// extract map from ts-node (ES6) output
-			const tsMap = convert.fromMapFileSource(code);
-
-			// ES6 -> ES5 using buble
-			const bubleResult = buble.transform(code, Object.assign({source: filename}, options));
-
-			// merge TS and buble maps
-			const tsAndBubleMap = merge(tsMap.sourcemap, bubleResult.map);
-
-			// transpile es2015 modules (import/export) using babel
-			const transpiledModules = babel.transform(bubleResult.code, {
-				sourceMaps: true,
-				presets: [],
-				plugins: ["transform-es2015-modules-commonjs"]
-			});
-
-			// merge babel map with previous map
-			const finalMap = merge(tsAndBubleMap, transpiledModules.map);
-
-			// join the final code with the final sourcemap
-			const codeWithoutMap = convert.removeMapFileComments(transpiledModules.code);
-			const sourceMapComment = convert.fromObject(finalMap).toComment();
-
-			compiled = codeWithoutMap + sourceMapComment;
-		} catch (err) {
-			if (err.snippet) {
-				console.log('Error compiling ' + filename + ':\n---');
-				console.log(err.snippet);
-				console.log(err.message);
-				console.log('')
-				process.exit(1);
-			}
-			throw err;
-		}
-		return _compile.call(this, compiled, filename);
-	}
-	return old(m, filename);
+var babelOptions = {
+	babelrc: false,
+	sourceMaps: true,
+	presets: [],
+	plugins: ["transform-es2015-modules-commonjs", "babel-plugin-inferno"]
 };
+
+registerExtension('.ts');
+registerExtension('.tsx');
+
+function registerExtension(ext) {
+
+	var old = require.extensions[ext];
+	if (!old) {
+		throw new Error('ts-node/register or equivalent compiler must be used before this compiler.')
+	}
+
+	require.extensions[ext] = function (m, filename) {
+
+		if (nodeModulesPattern.test(filename)) return old(m, filename);
+		var _compile = m._compile;
+		m._compile = function (code, fileName) {
+			var compiled;
+			try {
+				// extract map from ts-node (ES6) output
+				const tsMap = convert.fromMapFileSource(code);
+
+				// transpile es2015 modules (import/export) using babel
+				const babelResult = babel.transform(code, babelOptions);
+
+				// merge TS and babel maps
+				const tsAndBabelMap = merge(tsMap.sourcemap, babelResult.map);
+
+				// ES6 -> ES5 using buble
+				const bubleResult = buble.transform(babelResult.code, Object.assign({ source: filename }, bubleOptions));
+
+				// merge babel map with previous map
+				const finalMap = merge(tsAndBabelMap, bubleResult.map);
+
+				// join the final code with the final sourcemap
+				const codeWithoutMap = convert.removeMapFileComments(bubleResult.code);
+				const sourceMapComment = convert.fromObject(finalMap).toComment();
+
+				compiled = codeWithoutMap + sourceMapComment;
+			} catch (err) {
+				if (err.snippet) {
+					console.log('Error compiling ' + filename + ':\n---');
+					console.log(err.snippet);
+					console.log(err.message);
+					console.log('')
+					process.exit(1);
+				}
+				throw err;
+			}
+			return _compile.call(this, compiled, filename);
+		}
+		return old(m, filename);
+	};
+}
