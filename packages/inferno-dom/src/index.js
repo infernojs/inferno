584fbf7ee7b96f13223f5b6f518bca7b2f46d15d
Dominic Gannaway <dominicgannaway@Dominics-MacBook-Pro.local>
2016-10-12 16:58:43 +0100

    fixed a bad export on inferno-dom

--- packages/inferno-dom/src/index.js
+++ packages/inferno-dom/src/index.js
@@ -1,7 +1,7 @@
 import { render, findDOMNode, createRenderer } from '../../../src/DOM/rendering';
 import createStaticVElementClone from '../../../src/factories/createStaticVElementClone';
 import { disableRecycling } from '../../../src/DOM/recycling';
-import { initDevToolsHooks, exitDevToolsHooks }  from '../../../src/DOM/devtools';
+import { initDevToolsHooks }  from '../../../src/DOM/devtools';
 
 if (typeof window !== 'undefined' && window.document) {
 	initDevToolsHooks(window);
