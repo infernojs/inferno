7095544e8b467af962dd7447683554bfd8eafdf2
Ryan Megidov <ryan@megidov.com>
2016-10-25 20:02:07 +0200

    Export getRoutes

--- packages/inferno-router/src/index.js
+++ packages/inferno-router/src/index.js
@@ -1,9 +1,10 @@
 import Route from '../../../src/router/Route';
-import Router from '../../../src/router/Router';
+import Router, { getRoutes } from '../../../src/router/Router';
 import Link from '../../../src/router/Link';
 
 export default {
 	Route,
 	Router,
-	Link
+	Link,
+	getRoutes
 };
