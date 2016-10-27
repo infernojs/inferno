d7c3453bc35f61fafd8270fb24ab63c4db3fc71b
Ryan Megidov <ryan@megidov.com>
2016-10-27 08:56:11 +0200

    Add support for routes with decorators

--- src/router/Router.ts
+++ src/router/Router.ts
@@ -21,15 +21,21 @@ export function getRoutes(_routes, url, lastPath = '') {
 	for (let i = 0; i < routes.length; i++) {
 		const route = isArray(routes[i]) ? getRoutes(routes[i], url, lastPath) : routes[i];
 		const path = route.props.path || '/';
-		const children = route.props.children;
 		const newURL = url.replace('//', '/');
 		const fullPath = (lastPath + path).replace('//', '/');
 		const match = matchPath(false, fullPath, newURL);
 
 		if (match) {
 			route.props.params = match.params;
-			route.props.children = getRoutes(children, url, fullPath);
-			return route;
+			const children = route.props.children;
+			if (children) {
+				route.props.children = getRoutes(children, url, fullPath);
+			}
+			if (route.instance) {
+				return route.type(route.instance.props, route.instance.context);
+			} else {
+				return route;
+			}
 		}
 	}
 }
