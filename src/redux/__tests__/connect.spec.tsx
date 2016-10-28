4d3d271d82f7352cd9f328b63a8896a59764b692
Ryan Megidov <ryan@megidov.com>
2016-10-28 20:47:01 +0200

    Convert remaining redux tests to TSX

--- src/redux/__tests__/connect.spec.tsx
+++ src/redux/__tests__/connect.spec.tsx
@@ -1,10 +1,33 @@
+import { expect } from 'chai';
 import connect from './../connect';
 import { createStore } from 'redux';
 import Component from './../../component/es2015';
 import { render } from './../../DOM/rendering';
-import Inferno from '../../testUtils/inferno';
+import * as Inferno from '../../testUtils/inferno';
 Inferno; // suppress ts 'never used' error
-const sinon = require('sinon/pkg/sinon');
+
+class BasicComponent extends Component<any, any> {
+	props: any;
+	state: any;
+	render() {
+		return (
+			<div>{this.props.test}</div>
+		);
+	}
+}
+
+class BasicComponent1 extends Component<any, any> {
+	props: any;
+	state: any;
+	handleClick() {
+		this.props.action();
+	}
+	render() {
+		return (
+			<a onClick={this.handleClick.bind(this)}>{this.props.test}</a>
+		);
+	}
+}
 
 describe('connect', () => {
 	let container;
@@ -20,37 +43,19 @@ describe('connect', () => {
 		render(null, container);
 	});
 
-	class BasicComponent extends Component {
-		render() {
-			return (
-				<div>{this.props.test}</div>
-			);
-		}
-	}
-
-	class BasicComponent1 extends Component {
-		handleClick() {
-			this.props.action();
-		}
-
-		render() {
-			return (
-				<a onClick={this.handleClick.bind(this)}>{this.props.test}</a>
-			);
-		}
-	}
-
-	it('should return function', ()=> {
+	it('should return function', () => {
 		expect(connect()).to.be.a('function');
 	});
 
 	it('should have correct WrappedComponent', () => {
-		expect(connect()(BasicComponent).WrappedComponent).to.equal(BasicComponent);
+		const connectFunc = connect();
+		const ConnectedComponent = connectFunc(BasicComponent);
+		expect(ConnectedComponent.WrappedComponent).to.equal(BasicComponent);
 	});
 
 	it('should have correct mapStateToProps', () => {
 		const store = createStore(() => {
-			return {test: 1};
+			return { test: 1 };
 		});
 		const mapStateToProps = state => state;
 		const ConnectedComponent = connect(mapStateToProps)(BasicComponent);
@@ -59,65 +64,65 @@ describe('connect', () => {
 	});
 
 	it('should have correct mapDispatchToProps', () => {
-		const store = createStore((state = {test: 1}, action) => {
+		const store = createStore((state = { test: 1 }, action) => {
 			if (action && action.type === 'TEST_ACTION') {
-				return {test: 2};
+				return { test: 2 };
 			}
 			return state;
 		});
 		const mapDispatchToProps = dispatch => {
 			return {
 				action: () => {
-					dispatch({type: 'TEST_ACTION'});
+					dispatch({ type: 'TEST_ACTION' });
 				}
-			}
+			};
 		};
 		const mapStateToProps = state => state;
 		const ConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(BasicComponent1);
 		store.subscribe(() => {
 			render(<ConnectedComponent store={store}/>, container);
 		});
-		store.dispatch({type: ''});
+		store.dispatch({ type: '' } as any);
 		expect(container.innerHTML).to.equal('<a>1</a>');
 		container.querySelector('a').click();
 		expect(container.innerHTML).to.equal('<a>2</a>');
 	});
 
 	it('should have correct mapDispatchToProps', () => {
-		const store = createStore((state = {test: 1}, action) => {
+		const store = createStore((state = { test: 1 }, action) => {
 			if (action && action.type === 'TEST_ACTION') {
-				return {test: 2};
+				return { test: 2 };
 			}
 			return state;
 		});
 		const mapDispatchToProps = dispatch => {
 			return {
 				action: () => {
-					dispatch({type: 'TEST_ACTION'});
+					dispatch({ type: 'TEST_ACTION' });
 				}
-			}
+			};
 		};
 		const mapStateToProps = state => state;
 		const ConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(BasicComponent1);
 		store.subscribe(() => {
 			render(<ConnectedComponent store={store}/>, container);
 		});
-		store.dispatch({type: ''});
+		store.dispatch({ type: '' } as any);
 		expect(container.innerHTML).to.equal('<a>1</a>');
 		container.querySelector('a').click();
 		expect(container.innerHTML).to.equal('<a>2</a>');
 	});
 
 	it('should have correct mapDispatchToProps using action creators map', () => {
-		const store = createStore((state = {test: 1}, action) => {
+		const store = createStore((state = { test: 1 }, action) => {
 			if (action && action.type === 'TEST_ACTION') {
-				return {test: 2};
+				return { test: 2 };
 			}
 			return state;
 		});
 		const mapDispatchToProps = {
 			action: () => {
-				return {type: 'TEST_ACTION'};
+				return { type: 'TEST_ACTION' };
 			}
 		};
 		const mapStateToProps = state => state;
@@ -125,7 +130,7 @@ describe('connect', () => {
 		store.subscribe(() => {
 			render(<ConnectedComponent store={store}/>, container);
 		});
-		store.dispatch({type: ''});
+		store.dispatch({ type: '' } as any);
 		expect(container.innerHTML).to.equal('<a>1</a>');
 		container.querySelector('a').click();
 		expect(container.innerHTML).to.equal('<a>2</a>');
