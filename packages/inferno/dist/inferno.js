5a683992eaba686505a489535a6de9bb1e947680
Sampo Kivistö <sampo.kivisto@live.fi>
2016-11-13 16:33:50 +0200

    Finished patch errornous test, removed unreachable logic

--- packages/inferno/dist/inferno.js
+++ packages/inferno/dist/inferno.js
@@ -675,15 +675,8 @@ function patch(lastVNode, nextVNode, parentDom, lifecycle, context, isSVG) {
             }
         }
         else {
-            if (lastFlags) {
-                replaceLastChildAndUnmount(lastVNode, nextVNode, parentDom, lifecycle, context, isSVG);
-            }
-            else {
-                if (process.env.NODE_ENV !== 'production') {
-                    throwError(("patch() expects a valid VNode, instead it received an object with the type \"" + (typeof nextVNode) + "\"."));
-                }
-                throwError();
-            }
+            // Error case: mount new one replacing old one
+            replaceLastChildAndUnmount(lastVNode, nextVNode, parentDom, lifecycle, context, isSVG);
         }
     }
 }
