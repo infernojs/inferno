7487e8ed14512007c1d69a7bc332ed871a47f782
Sampo Kivistö <sampo.kivisto@live.fi>
2016-11-13 16:07:11 +0200

    Trying to reproduce edge cases in patch

---
+++ src/DOM/__tests__/patching.spec.ts
@@ -0,0 +1,67 @@
+import {expect} from 'chai';
+import {render} from './../rendering';
+import {createVNode, VNodeFlags, createTextVNode} from "../../core/shapes";
+
+describe('patching routine', () => {
+	let container;
+
+	beforeEach(() => {
+		container = document.createElement('div');
+	});
+
+	afterEach(() => {
+		container.innerHTML = '';
+	});
+
+	// TODO: Try to cover patching lastVNode !== nextVNode. requires no normalise and hoisting
+	it('Should do nothing if lastVNode strictly equals nextVnode', () => {
+		const vNode = createVNode(
+			VNodeFlags.HtmlElement,
+			'span',
+			null,
+			createVNode(
+				VNodeFlags.HtmlElement,
+				'span',
+				null,
+				createTextVNode('a'),
+				null,
+				null,
+				false
+			),
+			null,
+			null,
+			false
+		);
+
+		render(vNode, container);
+		expect(container.innerHTML).to.eql('<span><span>a</span></span>');
+
+		render(vNode, container);
+		expect(container.innerHTML).to.eql('<span><span>a</span></span>');
+	});
+
+	// TODO: Try to make lastVNode have invalid flags
+	it('Should mount nextNode if lastNode crashed', () => {
+		const nextNode = createVNode(
+			VNodeFlags.HtmlElement,
+			'span',
+			null,
+			createTextVNode('a'),
+			null,
+			null,
+			false
+		);
+		const lastNode = createVNode(0, 'span');
+
+		debugger;
+		try {
+			render(lastNode, container);
+		} catch (e) {
+			expect(e.message).to.eql('Inferno Error: mount() expects a valid VNode, instead it received an object with the type "object".');
+		}
+		expect(container.innerHTML).to.eql('');
+		debugger;
+		render(nextNode, container);
+		expect(container.innerHTML).to.eql('<span>a</span>')
+	});
+});
