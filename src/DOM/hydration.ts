08e08182650feb8d8d2a9926bb1fcf2e3570ae93
Dominic Gannaway <dg@domgan.com>
2016-11-21 22:34:26 +0000

    massively improved SSR performance, also changed how hydration works

--- src/DOM/hydration.ts
+++ src/DOM/hydration.ts
@@ -8,11 +8,13 @@ import {
 import {
 	createStatelessComponentInput,
 	createStatefulComponentInstance,
-	copyPropsTo
+	copyPropsTo,
+	replaceChild
 } from './utils';
 import {
 	mountStatelessComponentCallbacks,
-	mountStatefulComponentCallbacks
+	mountStatefulComponentCallbacks,
+	mountElement
 } from './mounting';
 import {
 	patchProp
@@ -88,35 +90,32 @@ function hydrateElement(vNode, dom, lifecycle, context, isSVG) {
 	const children = vNode.children;
 	const props = vNode.props;
 	const flags = vNode.flags;
-
-	vNode.dom = dom;
+	let rebuildVNode = false;
 
 	if (isSVG || (flags & VNodeFlags.SvgElement)) {
 		isSVG = true;
 	}
 	if (dom.tagName.toLowerCase() !== tag) {
-		if (process.env.NODE_ENV !== 'production') {
-			throwError(`hydrateElement() failed due to mismatch on DOM element tag name. Ensure server-side logic matches client side logic.`);
+		rebuildVNode = true;
+	} else {
+		vNode.dom = dom;
+		if (children) {
+			hydrateChildren(children, dom, lifecycle, context, isSVG);
 		}
-	}
-	if (children) {
-		hydrateChildren(children, dom, lifecycle, context, isSVG);
-	}
-	if (!(flags & VNodeFlags.HtmlElement)) {
-		processElement(flags, vNode, dom);
-	}
-	for (let prop in props) {
-		const value = props[prop];
+		if (!(flags & VNodeFlags.HtmlElement)) {
+			processElement(flags, vNode, dom);
+		}
+		for (let prop in props) {
+			const value = props[prop];
 
-		if (prop === 'key') {
-			// TODO: Maybe ?
-		} else if (prop === 'ref') {
-			// TODO: Maybe ?
-		} else if (prop === 'children') {
-			// TODO: Maybe ?
-		} else {
 			patchProp(prop, null, value, dom, isSVG);
 		}
+	}
+	if (rebuildVNode) {
+		const newDom = mountElement(vNode, null, lifecycle, context, isSVG);
+
+		vNode.dom = newDom;
+		replaceChild(dom.parentNode, newDom, dom);
 	}
 }
 
@@ -170,14 +169,9 @@ function hydrate(vNode, dom, lifecycle, context, isSVG) {
 }
 
 export default function hydrateRoot(input, parentDom, lifecycle) {
-	if (parentDom && parentDom.nodeType === 1) {
-		const rootNode = parentDom.querySelector('[data-infernoroot]');
-
-		if (rootNode && rootNode.parentNode === parentDom) {
-			rootNode.removeAttribute('data-infernoroot');
-			hydrate(input, rootNode, lifecycle, {}, false);
-			return true;
-		}
+	if (parentDom && parentDom.nodeType === 1 && parentDom.firstChild) {
+		hydrate(input, parentDom.firstChild, lifecycle, {}, false);
+		return true;
 	}
 	return false;
 }
