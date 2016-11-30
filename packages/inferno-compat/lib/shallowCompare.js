7f415890d8c0b1438bc65eaf543779877f82eba6
Dominic Gannaway <dominicgannaway@Dominics-MacBook-Pro.local>
2016-11-30 14:23:26 +0000

    converted to ES5

--- packages/inferno-compat/lib/shallowCompare.js
+++ packages/inferno-compat/lib/shallowCompare.js
@@ -1,11 +1,11 @@
 // Credited: https://github.com/tkh44/shallow-compare
 function shallowDiffers (a, b) {
-  for (let i in a) if (!(i in b)) return true
-  for (let i in b) if (a[i] !== b[i]) return true
+  for (var i in a) if (!(i in b)) return true
+  for (var i in b) if (a[i] !== b[i]) return true
   return false
 }
 
-export default (instance, nextProps, nextState) => {
+module.exports = function (instance, nextProps, nextState) {
   return (
     shallowDiffers(instance.props, nextProps) ||
     shallowDiffers(instance.state, nextState)
