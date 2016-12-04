b7beb6dc5d9580f9de36df55a67cc2c5a36a4b44
Luke Sheard <me@lukesheard.com>
2016-12-04 12:33:11 +0000

    Different browsers

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -131,19 +131,12 @@ module.exports = function (config) {
 			sl_chrome: {
 				base: 'SauceLabs',
 				browserName: 'chrome',
-				platform: 'Windows 7',
-				version: '35'
+				version: '54'
 			},
 			sl_firefox: {
 				base: 'SauceLabs',
 				browserName: 'firefox',
-				version: '30'
-			},
-			sl_ios_safari: {
-				base: 'SauceLabs',
-				browserName: 'iphone',
-				platform: 'OS X 10.9',
-				version: '7.1'
+				version: '45'
 			},
 			sl_ie_11: {
 				base: 'SauceLabs',
