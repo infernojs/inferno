eaac54d6c626c82b5488d930ba7467148047cb4b
Luke Sheard <me@lukesheard.com>
2016-12-04 13:23:26 +0000

    Karma setup and logging level

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,21 +1,22 @@
 /* global module */
+/* tslint:disable */
 module.exports = function (config) {
 	config.set({
 		// base path that will be used to resolve all patterns (eg. files, exclude)
-		basePath: '',
+		basePath: "",
 		// frameworks to use
 		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
 		frameworks: [
-			'chai',
-			'mocha'
+			"chai",
+			"mocha",
 		],
 		files: [
-			'./../node_modules/babel-polyfill/dist/polyfill.js',
-			'./../node_modules/sinon/pkg/sinon.js',
-			'./../src/**/*__tests__*/**/*.ts',
-			'./../src/**/*__tests__*/**/*.tsx',
-			'./../src/**/*__tests__*/**/*.js',
-			'./../src/**/*__tests__*/**/*.jsx'
+			"./../node_modules/babel-polyfill/dist/polyfill.js",
+			"./../node_modules/sinon/pkg/sinon.js",
+			"./../src/**/*__tests__*/**/*.ts",
+			"./../src/**/*__tests__*/**/*.tsx",
+			"./../src/**/*__tests__*/**/*.js",
+			"./../src/**/*__tests__*/**/*.jsx",
 		],
 		// Start these browsers, currently available:
 		// - Chrome
@@ -25,38 +26,38 @@ module.exports = function (config) {
 		// - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
 		// - PhantomJS
 		// - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-		browsers: ['Chrome'],
+		browsers: ["Chrome"],
 		customLaunchers: {
 			Chrome_travis_ci: {
-				base: 'Chrome',
-				flags: ['--no-sandbox']
-			}
+				base: "Chrome",
+				flags: ["--no-sandbox"],
+			},
 		},
 		// list of files to exclude
 		exclude: [],
 		preprocessors: {
-			'./../src/**/*__tests__*/**/*.ts': ['webpack'],
-			'./../src/**/*__tests__*/**/*.tsx': ['webpack'],
-			'./../src/**/*__tests__*/**/*.js': ['webpack'],
-			'./../src/**/*__tests__*/**/*.jsx': ['webpack']
+			"./../src/**/*__tests__*/**/*.ts": ["webpack"],
+			"./../src/**/*__tests__*/**/*.tsx": ["webpack"],
+			"./../src/**/*__tests__*/**/*.js": ["webpack"],
+			"./../src/**/*__tests__*/**/*.jsx": ["webpack"],
 		},
 		webpack: {
 			module: {
 				loaders: [
 					{
 						test: /\.tsx?$/,
-						loaders: ['babel-loader', 'ts-loader'],
-						exclude: /node_modules/
+						loaders: ["babel-loader", "ts-loader"],
+						exclude: /node_modules/,
 					}, {
 						test: /\.jsx?$/,
-						loader: 'babel-loader',
-						exclude: /node_modules/
-					}
-				]
+						loader: "babel-loader",
+						exclude: /node_modules/,
+					},
+				],
 			},
 			resolve: {
-				extensions: ['.js', '.jsx', '.ts', '.tsx']
-			}
+				extensions: [".js", ".jsx", ".ts", ".tsx"],
+			},
 		},
 		webpackMiddleware: {
 			noInfo: true,
@@ -88,13 +89,13 @@ module.exports = function (config) {
 				// Add the origins of chunks and chunk merging info
 				chunkOrigins: false,
 				// Add messages from child loaders
-				children: false
-			}
+				children: false,
+			},
 		},
 		// test results reporter to use
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['progress'],
+		reporters: ["progress"],
 
 		browserDisconnectTimeout: 10000,
 		browserDisconnectTolerance: 2,
@@ -112,13 +113,18 @@ module.exports = function (config) {
 		autoWatch: false,
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
-		singleRun: true
+		singleRun: true,
 	});
 
 	if (process.env.TRAVIS) {
-		config.browsers = ['Chrome_travis_ci'];
-		// Used by Travis to push coveralls info corretly to example coveralls.io
-		// Karma (with socket.io 1.x) buffers by 50 and 50 tests can take a long time on IEs;-)
-		config.browserNoActivityTimeout = 120000;
+		config.set({
+			browsers: [
+				"Chrome_travis_ci",
+			],
+			reporters: [
+				"failed",
+			],
+			browserNoActivityTimeout: 120000
+		});
 	}
 };
