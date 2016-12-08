e4a9ec59e554d5c0bdd4022d7e3c953f77a564d9
Luke Sheard <me@lukesheard.com>
2016-12-08 22:19:00 +0000

    Test on Chrome 54 only

--- config/sauceLaunchers.js
+++ config/sauceLaunchers.js
@@ -3,95 +3,104 @@ const config = {};
 /* =============
     CHROME
 ============== */
-['beta', '54', '53'].forEach((version) => {
-  // Windows
-  config['sl_chrome-win-' + version] = {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    platform: 'Windows 10',
-    version
-  };
+// ['beta', '54', '53'].forEach((version) => {
+//   // Windows
+//   config['sl_chrome-win-' + version] = {
+//     base: 'SauceLabs',
+//     browserName: 'chrome',
+//     platform: 'Windows 10',
+//     version
+//   };
 
-  // MAC
-  config['sl_chrome-osx-' + version] = {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    platform: 'OS X 10.11',
-    version
-  };
-});
+//   // MAC
+//   config['sl_chrome-osx-' + version] = {
+//     base: 'SauceLabs',
+//     browserName: 'chrome',
+//     platform: 'OS X 10.11',
+//     version
+//   };
+// });
 
-// LINUX
-['48', '47'].forEach((version) => {
-  config['sl_firefox-linux-' + version] = {
-    base: 'SauceLabs',
-    browserName: 'chrome',
-    platform: 'Linux',
-    version
-  };
-});
+// // LINUX
+// ['48', '47'].forEach((version) => {
+//   config['sl_firefox-linux-' + version] = {
+//     base: 'SauceLabs',
+//     browserName: 'chrome',
+//     platform: 'Linux',
+//     version
+//   };
+// });
 
-/* =============
-  FIREFOX
-============= */
-['beta', '50', '49'].forEach((version) => {
-  // Windows
-  config['sl_chrome-win-' + version] = {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    platform: 'Windows 10',
-    version
-  };
+// /* =============
+//   FIREFOX
+// ============= */
+// ['beta', '50', '49'].forEach((version) => {
+//   // Windows
+//   config['sl_chrome-win-' + version] = {
+//     base: 'SauceLabs',
+//     browserName: 'firefox',
+//     platform: 'Windows 10',
+//     version
+//   };
   
-  // MAC
-  config['sl_chrome-osx-' + version] = {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    platform: 'OS X 10.11',
-    version
-  };
-});
+//   // MAC
+//   config['sl_chrome-osx-' + version] = {
+//     base: 'SauceLabs',
+//     browserName: 'firefox',
+//     platform: 'OS X 10.11',
+//     version
+//   };
+// });
 
-// Linux
-['45', '44'].forEach((version) => {
-  config['sl_firefox-linux-' + version] = {
-    base: 'SauceLabs',
-    browserName: 'firefox',
-    platform: 'Linux',
-    version
-  };
-});
+// // Linux
+// ['45', '44'].forEach((version) => {
+//   config['sl_firefox-linux-' + version] = {
+//     base: 'SauceLabs',
+//     browserName: 'firefox',
+//     platform: 'Linux',
+//     version
+//   };
+// });
 
-/* =============
-  SAFARI
-=============== */
-['10', '9'].forEach((version) => {
-  config['sl_safair-linux-' + version] = {
-    base: 'SauceLabs',
-    browserName: 'safari',
-    platform: 'OS X 10.11',
-    version
-  };
-});
+// /* =============
+//   SAFARI
+// =============== */
+// ['10', '9'].forEach((version) => {
+//   config['sl_safair-linux-' + version] = {
+//     base: 'SauceLabs',
+//     browserName: 'safari',
+//     platform: 'OS X 10.11',
+//     version
+//   };
+// });
 
-/* ===============
-  EDGE
-================= */
-['14', '13'].forEach((version) => {
-  config['sl_edge-' + version] = {
-    base: 'SauceLabs',
-    browserName: 'MicrosoftEdge',
-    platform: 'Windows 10',
-    version
-  };
-});
+// /* ===============
+//   EDGE
+// ================= */
+// ['14', '13'].forEach((version) => {
+//   config['sl_edge-' + version] = {
+//     base: 'SauceLabs',
+//     browserName: 'MicrosoftEdge',
+//     platform: 'Windows 10',
+//     version
+//   };
+// });
 
-['8', '9', '10', '11'].forEach((version) => {
-  config['sl_ie-' + version] = {
+// ['8', '9', '10', '11'].forEach((version) => {
+//   config['sl_ie-' + version] = {
+//     base: 'SauceLabs',
+//     browserName: 'Internet Explorer',
+//     platform: 'Windows 7',
+//     version
+//   };
+// });
+
+[ 'Windows 10', 'OS X 10.11'].forEach((platform) => {
+  config['sl_chrome-54-' + platform] = {
     base: 'SauceLabs',
-    browserName: 'Internet Explorer',
-    platform: 'Windows 7',
-    version
+    browserName: 'chrome',
+    platform,
+    version: '54'
   };
 });
 
