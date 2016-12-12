adc27a20b4cfd0c6ebca48efb041522642c5a054
Luke Sheard <me@lukesheard.com>
2016-12-12 23:12:41 +0000

    Add Compatibility for IE

--- config/sauceLaunchers.js
+++ config/sauceLaunchers.js
@@ -94,6 +94,7 @@ const config = {};
     base: 'SauceLabs',
     browserName: 'Internet Explorer',
     platform: 'Windows 7',
+    'x-ua-compatible': 'IE=EmulateIE' + version,
     version
   };
 });
