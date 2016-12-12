3c56217d3edd3f76420c8d2bdf7bbe0b970fd39a
Sampo Kivistö <sampo.kivisto@live.fi>
2016-12-12 21:52:53 +0200

    fix test in Edge

--- src/DOM/__tests__/functional.spec.tsx
+++ src/DOM/__tests__/functional.spec.tsx
@@ -3,7 +3,6 @@ import Inferno, { createRenderer } from 'inferno';
 import { map, scan, reduce } from 'most';
 import { hold, sync } from 'most-subject';
 import { curry } from 'lodash/fp';
-import { innerHTML } from '../../tools/utils';
 import Type from 'union-type-es';
 Inferno; // suppress ts 'never used' error
 
@@ -58,15 +57,36 @@ describe('Functional methods (JSX)', () => {
 
 		runApp();
 		setTimeout(() => {
-			expect(
-				innerHTML(
-					container.innerHTML
-				)
-			).to.equal(
-				innerHTML(
-					'<div style="font-size: 48px; font-family: monospace; width: 100%; text-align: center;"><button id="decrement">-</button><div style="font-size: 48px; font-family: monospace; width: 100%; text-align: center;">0</div><button id="increment">+</button></div>'
-				)
-			);
+			/*
+			 <div style="font-size: 48px; font-family: monospace; width: 100%; text-align: center;">
+				 <button id="decrement">-</button>
+				 <div style="font-size: 48px; font-family: monospace; width: 100%; text-align: center;">0</div>
+				 <button id="increment">+</button>
+			 </div>
+			 */
+			const outerDiv = container.firstChild;
+
+			expect(outerDiv.style.fontSize).to.eql('48px');
+			expect(outerDiv.style.fontFamily).to.eql('monospace');
+			expect(outerDiv.style.width).to.eql('100%');
+			expect(outerDiv.style.textAlign).to.eql('center');
+			expect(outerDiv.tagName).to.eql('DIV');
+
+			const firstButton = outerDiv.childNodes[0];
+			expect(firstButton.getAttribute('id')).to.eql('decrement');
+			expect(firstButton.innerHTML).to.eql('-');
+
+			const midDiv = outerDiv.childNodes[1];
+			expect(midDiv.style.fontSize).to.eql('48px');
+			expect(midDiv.style.fontFamily).to.eql('monospace');
+			expect(midDiv.style.width).to.eql('100%');
+			expect(midDiv.style.textAlign).to.eql('center');
+			expect(midDiv.tagName).to.eql('DIV');
+			expect(midDiv.innerHTML).to.eql('0');
+
+			const secondButton = outerDiv.childNodes[2];
+			expect(secondButton.getAttribute('id')).to.eql('increment');
+			expect(secondButton.innerHTML).to.eql('+');
 			done();
 		}, 10);
 	});
