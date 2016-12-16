e8548c476d287cd74c57e212ee9a8a90f722e978
Luke Sheard <me@lukesheard.com>
2016-12-16 22:53:52 +0000

    Fix lint

--- examples/playground/playground.js
+++ examples/playground/playground.js
@@ -1,20 +1,20 @@
-(function() {
-	var t = Inferno.createElement;
+(function () {
+	let t = Inferno.createElement;
 
 	Inferno.render(t('div', null,
-		t('span', { className: 'foo'}, 'This is a span!')
+		t('span', { className: 'foo' }, 'This is a span!')
 	), document.getElementById('app'));
 
 	Inferno.render(t('header', null,
-		t('h1', { className: 'foo'}, 'This is a h1!')
+		t('h1', { className: 'foo' }, 'This is a h1!')
 	), document.getElementById('app'));
 
 	Inferno.render(t('header', null,
-		t('span', { className: 'foo'}, 'This is a span again!')
+		t('span', { className: 'foo' }, 'This is a span again!')
 	), document.getElementById('app'));
 
 	Inferno.render(t('header', null,
-		1, 	t('span', { className: 'foo'}, 'This is a span again!'), 2, 3
+		1, 	t('span', { className: 'foo' }, 'This is a span again!'), 2, 3
 	), document.getElementById('app'));
 
 })();
