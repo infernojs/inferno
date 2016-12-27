9b92364a1e5e7befed07aa4fa083d8b880338b1e
Luke Sheard <me@lukesheard.com>
2016-12-27 23:55:23 +0000

    working modules again

--- src/inferno.d.ts
+++ src/inferno.d.ts
@@ -1,4 +1,3 @@
-
 declare enum VNodeFlags {
 	Text = 1,
 	HtmlElement = 1 << 1,
@@ -20,29 +19,10 @@ declare enum VNodeFlags {
 	Component = ComponentFunction | ComponentClass | ComponentUnknown
 }
 
-type DOMNode = Node | SVGAElement;
 type Key = string | number | null;
 type Ref = Function | null;
-type Type = string | Function | null;
 type InfernoChildren = string | number | VNode | Array<string | VNode> | null;
-type InfernoInput = VNode | VNode[] | null | string | string[] | number | number[];
-
-interface Options {
-	recyclingEnabled: boolean;
-	findDOMNodeEnabled: boolean;
-	roots: any;
-	createVNode: Function | null;
-	beforeRender: Function | null;
-	afterRender: Function | null;
-	afterMount: Function | null;
-	afterUpdate: Function | null;
-	beforeUnmount: Function | null;
-}
-
-interface LinkedEventData {
-	data: any;
-	event: Function;
-}
+type Type = string | Function | null;
 
 interface Props {
 	children?: InfernoChildren;
@@ -64,84 +44,42 @@ interface VNode {
 }
 
 declare module 'inferno' {
-	export function linkEvent(data: any, event: Function): LinkedEventData;
-
-	// core shapes
-	export function createVNode(
-		flags: VNodeFlags,
-		type?: Type,
-		props?: Props,
-		children?: InfernoChildren,
-		events?,
-		key?: Key,
-		ref?: Ref,
-		noNormalise?: boolean
-	): VNode;
-
-	// cloning
-	export function cloneVNode(
-		vNodeToClone: VNode,
-		props?: Props,
-		..._children: InfernoChildren[]
-	): VNode;
-
-	// Shared items
+	export function createVNode(flags, type?, props?, children?, events?, key?, ref?, noNormalise?: boolean): any;
+	export function cloneVNode(node, props?, ...children);
+	export function render(...rest);
+	export function enableFindDOMNode();
+	export function findDOMNode(node: any): any;
+	export function createRenderer(...rest);
+	export function linkEvent(data, event: Function);
 	export const NO_OP;
+	export const ERROR_MSG;
 	export const EMPTY_OBJ;
+	interface Options {
+		recyclingEnabled: boolean;
+		findDOMNodeEnabled: boolean;
+		roots: any;
+		createVNode: Function | null;
+		beforeRender: Function | null;
+		afterRender: Function | null;
+		afterMount: Function | null;
+		afterUpdate: Function | null;
+		beforeUnmount: Function | null;
+	}
 
-	// DOM
-	export function render(input: InfernoInput, parentDom?: Node | SVGAElement): InfernoChildren;
-	export function findDOMNode(ref: DOMNode): DOMNode;
-	export function createRenderer(_parentDom: DOMNode): Function;
 	export const options: Options;
 }
 
-export interface ComponentLifecycle<P, S> {
-	componentDidMount?: () => void;
-	componentWillMount?(): void;
-	componentWillReceiveProps?(nextProps: P, nextContext: any): void;
-	shouldComponentUpdate?(nextProps: P, nextState: S, nextContext: any): boolean;
-	componentWillUpdate?(nextProps: P, nextState: S, nextContext: any): void;
-	componentDidUpdate?(prevProps: P, prevState: S, prevContext: any): void;
-	componentWillUnmount?: () => void;
-}
-
-export interface Mixin<P, S> extends ComponentLifecycle<P, S> {
-	statics?: {
-		[key: string]: any;
-	};
-
-	displayName?: string;
-	propTypes?: {[index: string]: Function};
-
-	getDefaultProps?(): P;
-	getInitialState?(): S;
-}
-
-export interface ComponentSpec<P, S> extends Mixin<P, S> {
-	mixins?: any;
-	[propertyName: string]: any;
-	render(props?, context?): any;
-}
-
-declare class Component<P, S> implements ComponentLifecycle<P, S>  {
-	refs?: any;
-	state?: S;
-	props?: P;
-	context?: any;
-	_vNode;
-	_unmounted?: boolean;
-	constructor (props?: P, context?);
-	componentWillReact();
-	componentWillReceiveProps? (nextProps: P, nextContext: Object): void;
-	componentWillUnmount();
-	forceUpdate (): void;
-	setState (v: Object, cb?: () => {}): boolean;
-	isPrototypeOf (v: Object): void;
-}
-
 declare module 'inferno-component' {
-	export default class Component<P, S> implements ComponentLifecycle<P, S>  {
+	interface ComponentLifecycle<P, S> {
+		componentDidMount?: () => void;
+		componentWillMount?(): void;
+		componentWillReceiveProps?(nextProps: P, nextContext: any): void;
+		shouldComponentUpdate?(nextProps: P, nextState: S, nextContext: any): boolean;
+		componentWillUpdate?(nextProps: P, nextState: S, nextContext: any): void;
+		componentDidUpdate?(prevProps: P, prevState: S, prevContext: any): void;
+		componentWillUnmount?(): void;
+	}
+	class Component<P, S> implements ComponentLifecycle<P, S>  {
 		refs?: any;
 		state?: S;
 		props?: P;
@@ -156,24 +94,140 @@ declare module 'inferno-component' {
 		setState (v: Object, cb?: () => {}): boolean;
 		isPrototypeOf (v: Object): void;
 	}
-}
-
-declare module 'inferno-create-class' {
-	export default function createClass<P, S>(obj: ComponentSpec<P, S>): Component<P, S>;
-}
-
-declare module 'inferno-hyperscript' {
-	export default function hyperscript(
-		_tag: string | VNode, 
-		_props?: any, 
-		_children?: InfernoChildren
-	): VNode;
+	export default Component;
 }
 
 declare module 'inferno-server' {
-	export function renderToString();
-	export function renderToStaticMarkup();
-	export function streamAsString();
-	export function streamAsStaticMarkup();
-	export function RenderStream();
+	export function renderToStaticMarkup(...rest);
+	export default function renderToString(...rest);
+}
+
+declare module 'inferno-create-class' {
+	export default function createClass(component: any): any;
+}
+
+declare module 'inferno-create-element' {
+	export default function createElement(component: any, props: any, ...children): any;
+}
+
+declare module 'inferno-hyperscript' {
+	export default function hyperscript(tag: any, props?: any, ...children): any;
+}
+
+declare module 'inferno-test-utils' {
+	type stringArr = string | string[];
+	
+	export function renderIntoDocument(element: VNode): VNode;
+	export function isElement(element: VNode): boolean;
+	export function isElementOfType(inst: VNode, componentClass: Function): boolean;
+	export function isDOMComponent(inst: any): boolean;
+	export function isDOMComponentElement(inst: VNode): boolean;
+	export function isCompositeComponent(inst): boolean;
+	export function isCompositeComponentWithType(inst, type: Function): boolean;
+	export function findAllInRenderedTree(inst: any, test: Function): VNode[];
+	export function scryRenderedDOMComponentsWithClass(root: VNode, classNames: stringArr): VNode[];
+	export function scryRenderedDOMComponentsWithTag (root: VNode, tagName: string): VNode[];
+	export function scryRenderedComponentsWithType (root: VNode, componentType: Function): VNode[];
+	export function findRenderedDOMComponentsWithClass(root: VNode, classNames: Function): VNode;
+	export function findenderedDOMComponentsWithTag(root: VNode, tagName: Function): VNode;
+	export function findRenderedComponentWithType(root: VNode, componentClass: Function): VNode;
+	export function mockComponent(module, mockTagName: string);
+
+	export default {
+		renderIntoDocument,
+		isElement,
+		isElementOfType,
+		isDOMComponent,
+		isDOMComponentElement,
+		isCompositeComponent,
+		isCompositeComponentWithType,
+		findAllInRenderedTree,
+		scryRenderedDOMComponentsWithClass,
+		scryRenderedDOMComponentsWithTag,
+		scryRenderedComponentsWithType,
+		findRenderedDOMComponentsWithClass,
+		findenderedDOMComponentsWithTag,
+		findRenderedComponentWithType,
+		mockComponent
+	};
+}
+
+declare module 'lodash/isPlainObject' {
+	function isPlainObject(value: any): boolean;
+	export = isPlainObject;
+}
+
+declare module 'invariant' {
+	function invariant(condition: any, message: string): void;
+	export = invariant;
+}
+
+declare module 'hoist-non-inferno-statics' {
+	function hoistStatics(connectClass: any, wrappedComponent: any): {[index: string]: any};
+	export = hoistStatics;
+}
+
+declare module 'concat-stream' {
+	function concatStream(func?: any);
+	export default concatStream;
+}
+
+declare module 'history' {
+	export function createBrowserHistory(options?: any);
+	export function createMemoryHistory(options?: any);
+}
+
+declare module 'mobx' {
+	export function toJS(value: any): any;
+	export function observable(value: any): any;
+	export function isObservable(value: any, property?: string): boolean;
+	export function extendObservable(...rest): any;
+	export class Reaction {
+		constructor(name?: string, onInvalidate?: any);
+		track(param: any): void;
+		runReaction();
+		dispose();
+		getDisposer(): any;
+	}
+	export const extras: any;
+}
+
+declare module 'redux' {
+	export function createStore(callback: any): any;
+	export function bindActionCreators(actionCreators: any, dispatch: any): any;
+}
+
+declare module 'sinon' {
+	export function spy(obj: any, event: string): any;
+	export function stub(obj: any, event: string, callback: any): any;
+	export const assert;
+}
+
+declare module 'most' {
+	export function map(f?: any, stream?: any): any;
+	export function reduce(f?: any, intitial?: any, stream?: any): any;
+	export function scan(f?: any, initial?: any, stream?: any): any;
+}
+
+declare module 'most-subject' {
+	export function hold(bufferSize?: number, subject?: any): any;
+	export function sync(): void;
+}
+
+declare module 'lodash/fp' {
+	export function curry(obj: any): any;
+}
+
+declare module 'union-type-es' {
+	export default function (obj: any): any;
+}
+
+interface Window { process: any; __karma__: any; mocha: any; }
+
+//noinspection TsLint
+declare namespace process {
+	//noinspection TsLint
+	export interface env {
+		NODE_ENV: any;
+	}
 }
