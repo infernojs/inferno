852006259546bf8e0db6e4fd8c1004334ad22ed3
Sampo Kivistö <sampo.kivisto@live.fi>
2017-02-08 21:38:50 +0200

    fix inferno-compat issues with Component construction

--- packages/inferno-compat/src/index.ts
+++ packages/inferno-compat/src/index.ts
@@ -2,9 +2,9 @@ import PropTypes from 'proptypes';
 import isValidElement from './isValidElement';
 import createClass from 'inferno-create-class';
 import infernoCreateElement from 'inferno-create-element';
-import { createVNode, render, findDOMNode, options, cloneVNode, VNode, InfernoChildren, Props } from 'inferno';
+import { createVNode, render, findDOMNode, options, cloneVNode, VNode, InfernoChildren, Props, EMPTY_OBJ } from 'inferno';
 import { NO_OP } from 'inferno-shared';
-import CoreComponent from 'inferno-component';
+import Component from 'inferno-component';
 import { ClassicComponentClass, ComponentSpec } from 'inferno-create-class';
 import _VNodeFlags from 'inferno-vnode-flags';
 
@@ -69,9 +69,7 @@ const Children = {
 	}
 };
 
-export class Component<P, S> extends CoreComponent<P, S> {
-	isReactComponent = {};
-}
+(Component.prototype as any).isReactComponent = {};
 
 let currentComponent = null;
 
@@ -187,11 +185,14 @@ function shallowDiffers(a, b): boolean {
 	return false;
 }
 
-export class PureComponent<P, S> extends Component<P, S> {
-	shouldComponentUpdate(props, state) {
-		return shallowDiffers(this.props, props) || shallowDiffers(this.state, state);
-	}
+function PureComponent(props, context) {
+	Component.call(this, props, context);
 }
+
+PureComponent.prototype = new Component({}, {});
+PureComponent.prototype.shouldComponentUpdate = function (props, state) {
+	return shallowDiffers(this.props, props) || shallowDiffers(this.state, state);
+};
 
 class WrapperComponent<P, S> extends Component<P, S> {
 	getChildContext() {
@@ -203,7 +204,7 @@ class WrapperComponent<P, S> extends Component<P, S> {
 	}
 }
 
-export function unstable_renderSubtreeIntoContainer(parentComponent, vNode, container, callback) {
+function unstable_renderSubtreeIntoContainer(parentComponent, vNode, container, callback) {
 	const wrapperVNode: VNode = createVNode(4, WrapperComponent, { context: parentComponent.context, children: vNode });
 	const component = render(wrapperVNode, container);
 
@@ -217,11 +218,11 @@ export function unstable_renderSubtreeIntoContainer(parentComponent, vNode, cont
 // Credit: preact-compat - https://github.com/developit/preact-compat
 const ELEMENTS = 'a abbr address area article aside audio b base bdi bdo big blockquote body br button canvas caption cite code col colgroup data datalist dd del details dfn dialog div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link main map mark menu menuitem meta meter nav noscript object ol optgroup option output p param picture pre progress q rp rt ruby s samp script section select small source span strong style sub summary sup table tbody td textarea tfoot th thead time title tr track u ul var video wbr circle clipPath defs ellipse g image line linearGradient mask path pattern polygon polyline radialGradient rect stop svg text tspan'.split(' ');
 
-export function createFactory(type) {
+function createFactory(type) {
 	return createElement.bind(null, type);
 }
 
-export const DOM = {};
+const DOM = {};
 for (let i = ELEMENTS.length; i--; ) {
 	DOM[ELEMENTS[i]] = createFactory(ELEMENTS[i]);
 }
@@ -236,6 +237,8 @@ export {
 	render,
 	isValidElement,
 	createElement,
+	Component,
+	PureComponent,
 	unmountComponentAtNode,
 	cloneElement,
 	PropTypes,
@@ -245,6 +248,10 @@ export {
 	cloneVNode,
 	NO_OP,
 	version,
+	unstable_renderSubtreeIntoContainer,
+	createFactory,
+	DOM,
+	EMPTY_OBJ
 };
 
 export default {
@@ -265,7 +272,8 @@ export default {
 	version,
 	unstable_renderSubtreeIntoContainer,
 	createFactory,
-	DOM
+	DOM,
+	EMPTY_OBJ
 };
 
 // To please the TS God
