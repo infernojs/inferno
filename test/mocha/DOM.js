22cba1d5059e8a8df199dfd63c0bbc4ece2013fb
Luke Sheard <me@lukesheard.com>
2017-03-16 21:56:07 +0000

    Sinon2fix (#942)
    * chore(package): update sinon to version 2.0.0
    
    https://greenkeeper.io/
    
    * Fix sinon merge issue
    
    * Fix test build bug

--- test/mocha/DOM.js
+++ test/mocha/DOM.js
@@ -1,22 +1,23 @@
 /* eslint-disable */
-var jsdom = require('jsdom');
+var jsdom = require("jsdom");
 
 // Setup the jsdom environment
 // @see https://github.com/facebook/react/issues/5046
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.document = jsdom.jsdom("<!doctype html><html><body></body></html>");
 global.window = document.defaultView;
 global.navigator = global.window.navigator;
 global.usingJSDOM = true;
+global.HTMLElement = global.window.HTMLElement;
 
-global.chai = require('chai');
+global.chai = require("chai");
 global.expect = global.chai.expect;
-global.sinon = require('sinon');
+global.sinon = require("sinon");
 
 //JSDOM doesn't support localStorage by default, so lets just fake it..
 if (!global.window.localStorage) {
 	global.window.localStorage = {
-		getItem() { return '{}'; },
-		setItem() {}
+		getItem() { return "{}"; },
+		setItem() {},
 	};
 }
 
@@ -27,8 +28,12 @@ propagateToGlobal(global.window);
 // from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
 function propagateToGlobal (window) {
 	for (var key in window) {
-		if (!window.hasOwnProperty(key)) continue;
-		if (key in global) continue;
+		if (!window.hasOwnProperty(key)) {
+			continue;
+		}
+		if (key in global) {
+			continue;
+		}
 
 		global[key] = window[key];
 	}
@@ -36,5 +41,5 @@ function propagateToGlobal (window) {
 if (!global.requestAnimationFrame) {
 	global.requestAnimationFrame = function (func) {
 		setTimeout(func, 1000 / 60);
-	}
+	};
 }
