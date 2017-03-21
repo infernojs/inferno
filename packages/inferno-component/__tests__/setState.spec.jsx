b0a5312350afdee0561d3c5f7b692d96d9f72984
Sampo Kivistö <sampo.kivisto@live.fi>
2017-03-21 21:35:49 +0200

    Add one more failing test case, cannot read flags of null Github #895

--- packages/inferno-component/__tests__/setState.spec.jsx
+++ packages/inferno-component/__tests__/setState.spec.jsx
@@ -59,7 +59,7 @@ describe('setState', () => {
 
 			render() {
 				const value = this.state.value;
-				return <TestComponent value={value} />;
+				return <TestComponent value={value}/>;
 			}
 		}
 
@@ -112,7 +112,7 @@ describe('setState', () => {
 
 			render() {
 				const value = this.state.value;
-				return <TestComponent value={value} />;
+				return <TestComponent value={value}/>;
 			}
 		}
 
@@ -156,8 +156,8 @@ describe('setState', () => {
 						<div>{this.state.foo}</div>
 						{
 							this.state.active ? (
-								<Child foo={this.state.foo} callback={this._setBar} />
-							) : <Child foo={this.state.foo} callback={this._setActive} />
+								<Child foo={this.state.foo} callback={this._setBar}/>
+							) : <Child foo={this.state.foo} callback={this._setActive}/>
 						}
 					</div>
 				);
@@ -190,7 +190,7 @@ describe('setState', () => {
 
 	// Render should work as per React
 	// https://jsfiddle.net/qb4ootgm/
-	it('Should fail during rendering', () => {
+	it('Should not fail during rendering', () => {
 		let doSomething;
 
 		class Parent extends Component {
@@ -222,12 +222,87 @@ describe('setState', () => {
 				return (
 					<div>
 						<div>{this.state.foo}</div>
-							<Child foo={this.state.foo} callback={this._setBar} />
-							<Child foo={this.state.foo} callback={this._setBar} />
-							<Child foo={this.state.foo} callback={this._setBar} />
+						<Child foo={this.state.foo} callback={this._setBar}/>
+						<Child foo={this.state.foo} callback={this._setBar}/>
+						<Child foo={this.state.foo} callback={this._setBar}/>
 					</div>
 				);
 			}
+		}
+
+		class Child extends Component {
+			constructor(props, context) {
+				super(props, context);
+			}
+
+			componentWillReceiveProps(nextProps) {
+				if (nextProps.foo !== 'bar') {
+					this.setState({
+						foo: 'bbaarr'
+					});
+
+					this.props.callback();
+				}
+			}
+
+			render() {
+				return (
+					<div>
+						<div>{this.props.foo}</div>
+					</div>
+				);
+			}
+		}
+
+		render(<Parent />, container);
+		doSomething();
+	});
+
+	it('Should not fail during rendering #2', () => {
+		let doSomething;
+
+		class Parent extends Component {
+			constructor(props, context) {
+				super(props, context);
+
+				this.state = {
+					active: false,
+					foo: 'b'
+				};
+
+				this._setBar = this._setBar.bind(this);
+				doSomething = this._setActive = this._setActive.bind(this);
+			}
+
+			_setBar() {
+				this.setState({
+					foo: 'bar'
+				});
+			}
+
+			_setActive() {
+				this.setState({
+					active: true
+				});
+			}
+
+			render() {
+				return (
+					<div>
+						<Child foo={this.state.foo} callback={this._setActive}/>
+						<ChildBar foo={this.state.foo} onComponentWillMount={this._setBar}/>
+						<ChildBar foo={this.state.foo}/>
+					</div>
+				);
+			}
+		}
+
+		function ChildBar({foo}) {
+			return (
+				<div>
+					{foo}
+				</div>
+			);
 		}
 
 		class Child extends Component {
