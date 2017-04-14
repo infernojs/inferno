b2788acf179ea86ba0a832ca0db56b3981946a7e
Sampo Kivistö <sampo.kivisto@live.fi>
2017-04-14 20:56:24 +0300

    fix connect parameter type and remove default value

--- packages/inferno-mobx/src/connect.ts
+++ packages/inferno-mobx/src/connect.ts
@@ -7,7 +7,7 @@ import makeReactive from './makeReactive';
 /**
  * Wraps a component and provides stores as props
  */
-function connect(arg1: any, arg2 = null): any {
+function connect(arg1: any, arg2?: any): any {
 	if (typeof arg1 === 'string') {
 		throwError('Store names should be provided as array');
 	}
