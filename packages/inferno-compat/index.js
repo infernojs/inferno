4145ee33a2ed7f54e0a7841e759faf14b74adb2c
Stefan <zerkalica@gmail.com>
2017-05-05 19:32:33 +0300

    extracted PropTypes to module (#1040)

--- packages/inferno-compat/index.js
+++ packages/inferno-compat/index.js
@@ -1,3 +1,2 @@
 module.exports = require('./dist').default;
 module.exports.default = module.exports;
-
---
+++ packages/inferno-compat/prop-types.js
@@ -0,0 +1,2 @@
+module.exports = require('./dist/PropTypes').default;
+module.exports.default = module.exports;
