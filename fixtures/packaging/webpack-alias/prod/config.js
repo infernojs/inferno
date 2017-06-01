826a7e6a8474687ed02a137494adfe6a93daa50b
Luke Sheard <me@lukesheard.com>
2017-06-01 21:46:05 +0100

    Use latest React packaging fixing

---
+++ fixtures/packaging/webpack-alias/prod/config.js
@@ -0,0 +1,15 @@
+var path = require('path');
+
+module.exports = {
+	entry: './input',
+	output: {
+		filename: 'output.js',
+	},
+	resolve: {
+		alias: {
+			'inferno': 'inferno/dist/inferno.min',
+			'inferno-create-element': 'inferno-create-element/dist/inferno-create-element.min',
+		},
+		root: path.resolve('../../../../packages'),
+	},
+};
