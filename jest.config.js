564602b585930acbcfefdcd37f1ef6fbb0b503a0
Luke Sheard <me@lukesheard.com>
2017-06-11 00:13:42 +0100

    Set coverage minimums

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,13 @@ module.exports = {
 	collectCoverageFrom: ['packages/*/src/**/*.ts', '!**/*.ts.js', '!**/inferno-utils/**/*', '!**/inferno-devtools/**/*' ],
 	coverageDirectory: 'coverage',
 	coverageReporters: ['html', 'lcov', 'text'],
-	globals: {
-		usingJSDOM: true,
-		usingJest: true
+	coverageThreshold: {
+		global: {
+			statements: 80,
+			branches: 75,
+			functions: 85,
+			lines: 85
+		}
 	},
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
 	moduleNameMapper: {
