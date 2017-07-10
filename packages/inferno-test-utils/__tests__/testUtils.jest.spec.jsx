4c4b8946ca1056f4f747944fea18283f0bacc562
Vitaly Logvinenko <mirenrich@gmail.com>
2017-07-10 19:09:10 +0300

    renderToSnapshot should return a snapshot with className prop (#1165)

--- packages/inferno-test-utils/__tests__/testUtils.jest.spec.jsx
+++ packages/inferno-test-utils/__tests__/testUtils.jest.spec.jsx
@@ -10,17 +10,27 @@ const FunctionalComponent = function(props) {
 
 describe("renderToSnapshot", () => {
   it("should return a snapshot from a valid vNode", () => {
-
     const snapshot = TestUtils.renderToSnapshot(
       <FunctionalComponent foo="bar" />
     );
 
-    if(usingJest){
+    if (usingJest) {
       expect(snapshot).toMatchSnapshot();
     } else {
       expect(snapshot.props.foo).toBeDefined();
       expect(snapshot.props.foo).toBe("bar");
     }
+  });
 
+  it("should return a snapshot with className prop", () => {
+    const TestComponent = () => <div className="test">Test</div>;
+
+    const snapshot = TestUtils.renderToSnapshot(<TestComponent />);
+
+    if (usingJest) {
+      expect(snapshot).toMatchSnapshot();
+    } else {
+      expect(snapshot.props.className).toBe("test");
+    }
   });
 });
