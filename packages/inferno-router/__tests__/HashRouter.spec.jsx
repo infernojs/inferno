5898178bc025486598f94ab628e88cc555829f83
Ryan Megidov <ryan@starcoders.com>
2017-09-15 21:12:08 +0200

    Add HashRouter and more tests

---
+++ packages/inferno-router/__tests__/HashRouter.spec.jsx
@@ -0,0 +1,39 @@
+import { render } from "inferno";
+import { innerHTML } from "inferno-utils";
+import { HashRouter } from "inferno-router";
+
+describe('A <HashRouter>', () => {
+  it('puts history on context.router', () => {
+    let history
+    const ContextChecker = (props, context) => {
+      history = context.router.history
+      return null
+    }
+
+    const node = document.createElement('div')
+
+    render((
+      <HashRouter>
+        <ContextChecker/>
+      </HashRouter>
+    ), node)
+
+    expect(typeof history).toBe('object')
+  })
+
+  it('warns when passed a history prop', () => {
+    const history = {}
+    const node = document.createElement('div')
+
+    spyOn(console, 'warn')
+
+    render((
+      <HashRouter history={history} />
+    ), node)
+
+    expect(console.warn).toHaveBeenCalledTimes(1)
+    expect(console.warn).toHaveBeenCalledWith(
+      expect.stringContaining('<HashRouter> ignores the history prop')
+    )
+  })
+})
