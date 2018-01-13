99c80ece3a2e385e72a96960373e839a2b73d588
Sampo Kivistö <sampo.kivisto@live.fi>
2018-01-13 13:54:23 +0200

    Fix commonjs entry file, production should point to minified file

--- packages/inferno-shared/index.js
+++ packages/inferno-shared/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/index.cjs.js');
-} else {
   module.exports = require('./dist/index.cjs.min.js');
+} else {
+  module.exports = require('./dist/index.cjs.js');
 }
