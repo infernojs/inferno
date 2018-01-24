a5a5483340eda741e6b18f3b871bc19c9bae2f8a
Sampo Kivistö <sampo.kivisto@live.fi>
2018-01-24 20:36:27 +0200

    trying to get travis working #1

--- fixtures/browser/karma.sauce.conf.js
+++ fixtures/browser/karma.sauce.conf.js
@@ -1,15 +1,7 @@
 const base = require('./karma.base.conf');
-const {
-  CI,
-  TRAVIS_BRANCH,
-  TRAVIS_BUILD_NUMBER,
-  TRAVIS_JOB_NUMBER,
-  TRAVIS_PULL_REQUEST
-} = process.env;
-
-const ci = String(CI).match(/^(1|true)$/gi);
-const pullRequest = !String(TRAVIS_PULL_REQUEST).match(/^(0|false|undefined)$/gi);
-const masterBranch = String(TRAVIS_BRANCH).match(/^master$/gi);
+const ci = String(process.env.CI).match(/^(1|true)$/gi);
+const pullRequest = !String(process.env.TRAVIS_PULL_REQUEST).match(/^(0|false|undefined)$/gi);
+const masterBranch = String(process.env.TRAVIS_BRANCH).match(/^master$/gi);
 const sauce = ci && !pullRequest && masterBranch;
 const varToBool = (sVar) => !!String(sVar).match('true');
 
@@ -44,9 +36,11 @@ module.exports = function(config) {
       'saucelabs'
     ],
     sauceLabs: {
-      testName: 'Inferno Browser Karma Tests: ' + TRAVIS_JOB_NUMBER,
-      build: (TRAVIS_JOB_NUMBER || 'Local'),
-      tags: [(TRAVIS_BRANCH || 'master')]
+      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
+      testName: 'Inferno Browser Karma Tests: ' + process.env.TRAVIS_JOB_NUMBER,
+      build: (process.env.TRAVIS_JOB_NUMBER || 'Local'),
+      startConnect: false,
+      tags: [(process.env.TRAVIS_BRANCH || 'master')]
     },
     customLaunchers: sauceLaunchers.launchers,
     browsers: sauceLaunchers.browsers,
