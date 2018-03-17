7ef19a17f41d1f2c31ba155b6e5ec005de423d11
Sampo Kivistö <sampo.kivisto@live.fi>
2018-03-17 10:36:46 +0200

    updated uibench dependencies

--- benchmarks/uibench/webpack.config.js
+++ benchmarks/uibench/webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     publicPath: __dirname,
     filename: 'bundle.js'
   },
+  mode: "production",
   module: {
     rules: [
       {
@@ -54,8 +55,7 @@ module.exports = {
     mainFields: ['module', 'main']
   },
   plugins: [
-    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } })
   ],
   devServer: {
     port: process.env.PORT || 8000
