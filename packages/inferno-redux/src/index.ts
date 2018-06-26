a9ad7d359e9a758c12540bd72b5e96d1de41a8bd
Sampo Kivistö <sampo.kivisto@live.fi>
2018-06-26 20:34:01 +0300

    Exported wrapActionCreators method

--- packages/inferno-redux/src/index.ts
+++ packages/inferno-redux/src/index.ts
@@ -2,5 +2,6 @@ import { Dispatch } from 'redux';
 import { connectAdvanced, IConnectOptions } from './components/connectAdvanced';
 import { Provider } from './components/Provider';
 import { connect } from './connect/connect';
+import { wrapActionCreators } from './utils/wrapActionCreators';
 
-export { Provider, connectAdvanced, connect, IConnectOptions, Dispatch };
+export { Provider, connectAdvanced, connect, IConnectOptions, Dispatch, wrapActionCreators };
