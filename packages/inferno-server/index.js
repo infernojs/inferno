6be1d523488154489ed30e8746cb7541d23b62f1
Sampo Kivistö <sampo.kivisto@live.fi>
2018-07-02 20:56:56 +0300

    Added development mode warning

--- packages/inferno-server/index.js
+++ packages/inferno-server/index.js
@@ -3,7 +3,5 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./dist/index.cjs.min.js');
 } else {
-  console.info("Inferno server is in development mode.");
-
   module.exports = require('./dist/index.cjs.js');
 }
