29c566f3167b98b18e17a1846b5cbc5eac8b555e
Sampo Kivistö <sampo.kivisto@live.fi>
2018-08-18 12:18:08 +0300

    removed debugger

--- packages/inferno-create-element/__tests__/components.spec.js
+++ packages/inferno-create-element/__tests__/components.spec.js
@@ -42,7 +42,6 @@ describe('Components (non-JSX)', () => {
     }
 
     it('should recover from a basic error', () => {
-      debugger
       const template = (Component, title) =>
         createElement(
           'div',
