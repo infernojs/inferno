ef8ea627c8de08763ec5fa7e69194b3684bdd1b1
Sergey Kurdin <sergey@ski.com>
2018-08-28 15:03:52 -0400

    removing word undefined in mbox lodger when rootNodeID is not defined

--- packages/inferno-mobx/src/observer.ts
+++ packages/inferno-mobx/src/observer.ts
@@ -103,7 +103,7 @@ const reactiveMixin = {
 
     // Generate friendly name for debugging
     const initialName = this.displayName || this.name || (this.constructor && (this.constructor.displayName || this.constructor.name)) || '<component>';
-    const rootNodeID = this._reactInternalInstance && this._reactInternalInstance._rootNodeID;
+    const rootNodeID = (this._reactInternalInstance && this._reactInternalInstance._rootNodeID) || null;
 
     /**
      * If props are shallowly modified, react will render anyway,
@@ -151,7 +151,7 @@ const reactiveMixin = {
     let isRenderingPending = false;
 
     const initialRender = () => {
-      reaction = new Reaction(`${initialName}#${rootNodeID}.render()`, () => {
+      reaction = new Reaction(`${initialName}${!rootNodeID ? '' : `#${rootNodeID}`}.render()`, () => {
         if (!isRenderingPending) {
           // N.B. Getting here *before mounting* means that a component constructor has side effects (see the relevant test in misc.js)
           // This unidiomatic React usage but React will correctly warn about this so we continue as usual
