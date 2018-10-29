427ba3b80a9b4c910a8e6e764fd2c09b8556ac1d
makepost <makepost@firemail.cc>
2018-10-29 19:43:05 +0200

    CreateElement: Validate prop types (#1408)
    Mentioned in #1407.

--- packages/inferno-create-element/__tests__/createElementTyped.spec.tsx
+++ packages/inferno-create-element/__tests__/createElementTyped.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from 'inferno';
+import { Component, render } from 'inferno';
 import { createElement } from 'inferno-create-element';
 
 describe('CreateElement (non-JSX)', () => {
@@ -62,5 +62,45 @@ describe('CreateElement (non-JSX)', () => {
         );
 
         render(App(), container);
+    });
+
+    it('Should check component props', () => {
+        class App extends Component<{ className: string }, any> {
+            public render() {
+                return createElement(
+                    'div',
+                    { className: this.props.className },
+                    createElement('div', { className: 'title' }, 'Example'),
+                    createElement('hr')
+                );
+            }
+        }
+
+        render(createElement(App, { className: "App" }), container);
+
+        /** Should be an error if uncommented: */
+        // render(createElement(App, { className: 1 }), container);
+        // render(createElement(App, {}), container);
+
+        /** Would like to be an error but it'd break createElement('hr'): */
+        render(createElement(App), container);
+    });
+
+    it('Should check functional component props', () => {
+        const App = ({ className }: { className: string }) => createElement(
+            'div',
+            { className },
+            createElement('div', { className: 'title' }, 'Example'),
+            createElement('hr')
+        );
+
+        render(createElement(App, { className: "App" }), container);
+
+        /** Should be an error if uncommented: */
+        // render(createElement(App, { className: 1 }), container);
+        // render(createElement(App, {}), container);
+
+        /** Would like to be an error but it'd break createElement('hr'): */
+        render(createElement(App), container);
     });
 });
