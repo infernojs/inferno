aa8d5800793ad7b63aa7c3c8cbed115ea45b9cce
jakubroch <roch.jakub@qualaroo.com>
2019-02-18 17:26:19 +0100

    feat: remove unnecessary ownerDoc read

--- packages/inferno/src/DOM/utils/innerhtml.ts
+++ packages/inferno/src/DOM/utils/innerhtml.ts
@@ -1,11 +1,5 @@
 export function isSameInnerHTML(dom: Element, innerHTML: string): boolean {
-  let doc = dom.ownerDocument;
-
-  if (!doc) {
-    doc = document;
-  }
-
-  const tempdom = doc.createElement('i');
+  const tempdom = document.createElement('i');
 
   tempdom.innerHTML = innerHTML;
   return tempdom.innerHTML === dom.innerHTML;
