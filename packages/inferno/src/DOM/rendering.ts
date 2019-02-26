8137713d2bd707c765cc1a5172cced8b331d5e0f
jakubroch <roch.jakub@qualaroo.com>
2019-02-26 12:19:46 +0100

    fix: tslint annotations

--- packages/inferno/src/DOM/rendering.ts
+++ packages/inferno/src/DOM/rendering.ts
@@ -47,7 +47,7 @@ export function __render(
   }
   const lifecycle: Function[] = [];
   let rootInput = (parentDOM as any).$V as VNode | null;
-  const doc = (parentDOM || document).ownerDocument || document;
+  const doc = parentDOM as HTMLDocument;
 
   if (getDocumentId(doc) === null) {
     registerDocumentScope(doc);
