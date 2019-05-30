6c542041f074beedfab75973a5c9a2ea576201f5
Sampo Kivistö <sampo.kivisto@live.fi>
2019-05-30 20:04:34 +0300

    Replaced deprecated rollup node-resolve option.jsnext with mainfield setting

--- scripts/rollup/plugins/index.js
+++ scripts/rollup/plugins/index.js
@@ -11,7 +11,7 @@ module.exports = function(version, options) {
     aliasPlugin,
     nodeResolve({
       extensions: ['.ts', '.js', '.json'],
-      jsnext: true
+      mainFields: ['module', 'main']
     }),
     commonjs({
       include: 'node_modules/**'
