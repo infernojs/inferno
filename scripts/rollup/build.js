550550a91613a045b11c9326758b402ed4f19d4d
jhsware <sebastian@urbantalk.se>
2021-04-14 12:48:27 +0200

    Refactor build -- 13x faster typescript bundling (#1558)
    * Rewrote build scripts for readability
    
    * Fixed error function and feedback
    
    * Fixed output names
    
    * Only perform type generation once since it is done for the entire project anyway
    
    * Bundle inferno-shared first because it has dependents. Stream feedback as it comes
    
    * We can use types generated by rollup
    
    * Missed setting production on umd prod build
    
    * Allow partial builds and re-added quick-test scripts
    
    * Much faster build by transpiling typescript before bundling
    
    * Use tmpDist as temporary folder instead
    
    * show build size on build
    
    * Fixed quick-test scripts
    
    * Removed broken quick-test scripts.

--- scripts/rollup/build.js
+++ scripts/rollup/build.js
@@ -1,47 +1,97 @@
-const { mkdir } = require('fs');
+const { promises: { mkdir: mkdirAsync }, lstatSync, readdirSync } = require('fs');
 const { join } = require('path');
+const createPlugins = require('./plugins');
+const { rollup } = require('rollup');
+
 
 const cwd = process.cwd();
+const ROOT = join(__dirname, '../../packages');
+
 const pkgJSON = require(join(cwd, 'package.json'));
 
 if (pkgJSON.private || !pkgJSON.rollup) {
   return;
 }
 
-mkdir(join(cwd, 'dist'), (err) => {
-  if (err && err.code !== 'EEXIST') {
-    throw Error(e);
-  }
 
-  var exit = process.exit;
+const args = require('minimist')(process.argv.slice(2));
 
-  const options = require('minimist')(process.argv.slice(2), {
-    boolean: ['replace', 'optimize', 'minify'],
-    default: {
-      env: 'development',
-      ext: '.js',
-      format: 'umd',
-      name: pkgJSON.name,
-      replace: true,
-      minify: true,
-      version: pkgJSON.version
+const moduleGlobals = readdirSync(ROOT)
+  .filter((path) => lstatSync(join(ROOT, path)).isDirectory())
+  .reduce((acc, pkgName) => {
+    const pkgJSON = require(join(ROOT, pkgName, 'package.json'));
+
+    if (pkgJSON.rollup && pkgJSON.rollup.moduleName) {
+      acc[pkgJSON.name] = pkgJSON.rollup.moduleName;
+    }
+
+    return acc;
+  }, {});
+
+// Self calling function to allow async/await for readability
+(async () => {
+
+  // Create dist folder
+  await mkdirAsync(join(cwd, 'dist')).catch(err => {
+    if (err.code !== 'EEXIST') {
+      throw Error(err);
     }
   });
-
-  const createRollup = require('./rollup');
-  const createBundle = require('./bundle');
-
-  const rollup = createRollup(options);
-  const bundle = createBundle(options);
-
-  rollup
-    .then(bundle)
-    .then(() => {
-      console.log(`${pkgJSON.name} in ${options.format} is DONE`);
-    })
-    .catch((error) => {
+  
+  // Get info from package.json
+  const {
+    version,
+    rollup: rollupConfig = {},
+    dependencies = {}, devDependencies = {}, peerDependencies = {}
+  } = pkgJSON;
+  
+  // Figure out from package.json what dependencies to bundle
+  function exclusionFilter(name) {
+    return !(rollupConfig.bundledDependencies || []).includes(name);
+  }
+  const deps = Object.assign({}, devDependencies, peerDependencies, dependencies);
+  const external = Object.keys(deps)
+    .filter(exclusionFilter)
+    .filter(function (elem, index, self) {
+      return index === self.indexOf(elem);
+    });
+  
+  // The import stream is NodeJS specific and should alwats be marked as external
+  // Used in inferno-server
+  external.push('stream');
+  
+  const defaultOptions = {
+    name: 'index',
+    replace: true,
+    version: pkgJSON.version
+  }
+  
+  const targets = [
+    //esmDev --name=index --ext=.dev.esm.js --env=development --format=es --minify=false
+    Object.assign({}, defaultOptions, { env: 'development', format: 'es', minify: false, ext: '.dev.esm.js'}),
+    //esmProd --name=index --ext=.esm.js --env=production --format=es --minify=false
+    Object.assign({}, defaultOptions, { env: 'production', format: 'es', minify: false, ext: '.esm.js'}),
+    //esNext --name=index --ext=.esnext.js --env=production --format=es --esnext=true --minify=false
+    Object.assign({}, defaultOptions, { env: 'production', format: 'es', esnext: true, minify: false, ext: '.esnext.js'}),
+    //cjsDev --env=development --format=cjs --replace=true --name=index.cjs --minify=false
+    Object.assign({}, defaultOptions, { env: 'development', format: 'cjs', minify: false, ext: '.cjs.js'}),
+    //cjsProd --env=production --format=cjs --replace=true --name=index.cjs --minify=true --ext=.min.js
+    Object.assign({}, defaultOptions, { env: 'production', format: 'cjs', minify: true, ext: '.cjs.min.js'}),
+    //umdDev --minify=false
+    Object.assign({}, defaultOptions, { env: 'development', format: 'umd', minify: false, name: pkgJSON.name, ext: '.js'}),
+    //umdProd --env=production --ext=.min.js
+    Object.assign({}, defaultOptions, { env: 'production', format: 'umd', minify: true, name: pkgJSON.name, ext: '.min.js'}),
+  ].filter((target) => {
+    if (args['target-ext']) {
+      return target.ext === args['target-ext'];
+    }
+    return true
+  })
+  
+  await targets.forEach(async (options) => {
+    const errorFunc = (error) => {
       console.error(error); // Print whole error object
-
+    
       if (error.snippet) {
         console.error('\u001b[31;1m');
         console.error('\n-------- Details -------');
@@ -52,8 +102,49 @@ mkdir(join(cwd, 'dist'), (err) => {
         console.error('\n-------------------------');
         console.error('\u001b[0m');
       }
-
+    
       console.error(`${pkgJSON.name} in ${options.format} is FAILED ${error.message}`);
-      exit(-1); // Do not continue build in case of error to avoid publishing garbage, Github #1157
-    });
-});
+      process.exit(-1); // Do not continue build in case of error to avoid publishing garbage, Github #1157
+    };    
+
+    // Minify settings are found in plugins/index.js
+    const rollupPlugins = createPlugins(version, options);
+    
+    // Transform
+    const { write } = await rollup({
+      input: join(cwd, 'tmpDist/index.js'),
+      external: external,
+      plugins: rollupPlugins
+    }).catch(errorFunc);
+  
+    // Write bundle
+    const filename = `${options.name}${options.ext}`;
+
+    // TODO: Consider adding globals to avoid warnings:
+    // No name was provided for external module 'stream' in output.globals – guessing 'stream'
+    // No name was provided for external module 'history' in output.globals – guessing 'history'
+    // No name was provided for external module 'path-to-regexp-es6' in output.globals – guessing 'pathToRegexp'
+    // No name was provided for external module 'hoist-non-inferno-statics' in output.globals – guessing 'hoistNonReactStatics'
+    // No name was provided for external module 'redux' in output.globals – guessing 'redux'
+    // No name was provided for external module 'mobx' in output.globals – guessing 'mobx'
+  
+    const bundleOptions = {
+      output: {
+        file: `dist/${filename}`,
+        format: options.format,
+        globals: Object.assign(moduleGlobals, rollupConfig.moduleGlobals),
+        name: rollupConfig.moduleName,
+        indent: true,
+        extend: true,
+        sourcemap: false
+      }
+    };
+  
+    if (options.format === 'cjs') {
+      bundleOptions.output.exports = 'named';
+    }
+  
+    await write(bundleOptions).catch(errorFunc);
+    console.log(`${pkgJSON.name} in ${options.name}${options.ext} is DONE`);
+  });
+})();
--- scripts/rollup/plugins/alias.js
+++ scripts/rollup/plugins/alias.js
@@ -5,18 +5,18 @@ const ROOT = join(__dirname, '../../../');
 module.exports = alias({
   resolve: ['.js'],
   entries: [
-    { find: 'inferno', replacement: resolve(ROOT, 'packages/inferno/dist/index.esm.js') },
-    { find: 'inferno-animation', replacement: resolve(ROOT, 'packages/inferno-animation/dist/index.esm.js') },
-    { find: 'inferno-compat', replacement: resolve(ROOT, 'packages/inferno-compat/dist/index.esm.js') },
-    { find: 'inferno-create-class', replacement: resolve(ROOT, 'packages/inferno-create-class/dist/index.esm.js') },
-    { find: 'inferno-create-element', replacement: resolve(ROOT, 'packages/inferno-create-element/dist/index.esm.js') },
-    { find: 'inferno-hyperscript', replacement: resolve(ROOT, 'packages/inferno-hyperscript/dist/index.esm.js') },
-    { find: 'inferno-mobx', replacement: resolve(ROOT, 'packages/inferno-mobx/dist/index.esm.js') },
-    { find: 'inferno-redux', replacement: resolve(ROOT, 'packages/inferno-redux/dist/index.esm.js') },
-    { find: 'inferno-router', replacement: resolve(ROOT, 'packages/inferno-router/dist/index.esm.js') },
-    { find: 'inferno-server', replacement: resolve(ROOT, 'packages/inferno-server/dist/index.esm.js') },
-    { find: 'inferno-shared', replacement: resolve(ROOT, 'packages/inferno-shared/dist/index.esm.js') },
-    { find: 'inferno-clone-vnode', replacement: resolve(ROOT, 'packages/inferno-clone-vnode/dist/index.esm.js') },
-    { find: 'inferno-hydrate', replacement: resolve(ROOT, 'packages/inferno-hydrate/dist/index.esm.js') }
+    { find: 'inferno', replacement: resolve(ROOT, 'packages/inferno/tmpDist/index.js') },
+    { find: 'inferno-animation', replacement: resolve(ROOT, 'packages/inferno-animation/tmpDist/index.js') },
+    { find: 'inferno-compat', replacement: resolve(ROOT, 'packages/inferno-compat/tmpDist/index.js') },
+    { find: 'inferno-create-class', replacement: resolve(ROOT, 'packages/inferno-create-class/tmpDist/index.js') },
+    { find: 'inferno-create-element', replacement: resolve(ROOT, 'packages/inferno-create-element/tmpDist/index.js') },
+    { find: 'inferno-hyperscript', replacement: resolve(ROOT, 'packages/inferno-hyperscript/tmpDist/index.js') },
+    { find: 'inferno-mobx', replacement: resolve(ROOT, 'packages/inferno-mobx/tmpDist/index.js') },
+    { find: 'inferno-redux', replacement: resolve(ROOT, 'packages/inferno-redux/tmpDist/index.js') },
+    { find: 'inferno-router', replacement: resolve(ROOT, 'packages/inferno-router/tmpDist/index.js') },
+    { find: 'inferno-server', replacement: resolve(ROOT, 'packages/inferno-server/tmpDist/index.js') },
+    { find: 'inferno-shared', replacement: resolve(ROOT, 'packages/inferno-shared/tmpDist/index.js') },
+    { find: 'inferno-clone-vnode', replacement: resolve(ROOT, 'packages/inferno-clone-vnode/tmpDist/index.js') },
+    { find: 'inferno-hydrate', replacement: resolve(ROOT, 'packages/inferno-hydrate/tmpDist/index.js') }
   ]
 });
