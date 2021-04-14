dd7eb333d0684b66eda6a5900087debabe75bf1f
jhsware <sebastian@urbantalk.se>
2021-04-14 10:57:34 +0200

    Use tmpDist as temporary folder instead

--- scripts/rollup/build.js
+++ scripts/rollup/build.js
@@ -112,7 +112,7 @@ const moduleGlobals = readdirSync(ROOT)
     
     // Transform
     const { write } = await rollup({
-      input: join(cwd, 'lib/index.js'),
+      input: join(cwd, 'tmpDist/index.js'),
       external: external,
       plugins: rollupPlugins
     }).catch(errorFunc);
--- scripts/rollup/plugins/alias.js
+++ scripts/rollup/plugins/alias.js
@@ -5,18 +5,18 @@ const ROOT = join(__dirname, '../../../');
 module.exports = alias({
   resolve: ['.js'],
   entries: [
-    { find: 'inferno', replacement: resolve(ROOT, 'packages/inferno/lib/index.js') },
-    { find: 'inferno-animation', replacement: resolve(ROOT, 'packages/inferno-animation/lib/index.js') },
-    { find: 'inferno-compat', replacement: resolve(ROOT, 'packages/inferno-compat/lib/index.js') },
-    { find: 'inferno-create-class', replacement: resolve(ROOT, 'packages/inferno-create-class/lib/index.js') },
-    { find: 'inferno-create-element', replacement: resolve(ROOT, 'packages/inferno-create-element/lib/index.js') },
-    { find: 'inferno-hyperscript', replacement: resolve(ROOT, 'packages/inferno-hyperscript/lib/index.js') },
-    { find: 'inferno-mobx', replacement: resolve(ROOT, 'packages/inferno-mobx/lib/index.js') },
-    { find: 'inferno-redux', replacement: resolve(ROOT, 'packages/inferno-redux/lib/index.js') },
-    { find: 'inferno-router', replacement: resolve(ROOT, 'packages/inferno-router/lib/index.js') },
-    { find: 'inferno-server', replacement: resolve(ROOT, 'packages/inferno-server/lib/index.js') },
-    { find: 'inferno-shared', replacement: resolve(ROOT, 'packages/inferno-shared/lib/index.js') },
-    { find: 'inferno-clone-vnode', replacement: resolve(ROOT, 'packages/inferno-clone-vnode/lib/index.js') },
-    { find: 'inferno-hydrate', replacement: resolve(ROOT, 'packages/inferno-hydrate/lib/index.js') }
+    { find: 'inferno', replacement: resolve(ROOT, 'packages/inferno/tmpDist/index.js') },
+    { find: 'inferno-animation', replacement: resolve(ROOT, 'packages/inferno-animation/tmpDist/index.js') },
+    { find: 'inferno-compat', replacement: resolve(ROOT, 'packages/inferno-compat/tmpDist/index.js') },
+    { find: 'inferno-create-class', replacement: resolve(ROOT, 'packages/inferno-create-class/tmpDist/index.js') },
+    { find: 'inferno-create-element', replacement: resolve(ROOT, 'packages/inferno-create-element/tmpDist/index.js') },
+    { find: 'inferno-hyperscript', replacement: resolve(ROOT, 'packages/inferno-hyperscript/tmpDist/index.js') },
+    { find: 'inferno-mobx', replacement: resolve(ROOT, 'packages/inferno-mobx/tmpDist/index.js') },
+    { find: 'inferno-redux', replacement: resolve(ROOT, 'packages/inferno-redux/tmpDist/index.js') },
+    { find: 'inferno-router', replacement: resolve(ROOT, 'packages/inferno-router/tmpDist/index.js') },
+    { find: 'inferno-server', replacement: resolve(ROOT, 'packages/inferno-server/tmpDist/index.js') },
+    { find: 'inferno-shared', replacement: resolve(ROOT, 'packages/inferno-shared/tmpDist/index.js') },
+    { find: 'inferno-clone-vnode', replacement: resolve(ROOT, 'packages/inferno-clone-vnode/tmpDist/index.js') },
+    { find: 'inferno-hydrate', replacement: resolve(ROOT, 'packages/inferno-hydrate/tmpDist/index.js') }
   ]
 });
