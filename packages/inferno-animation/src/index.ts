2a227bdcf5dd31cae01ee5700971e88a5169fe8b
jhsware <sebastian@urbantalk.se>
2021-07-15 15:47:15 +0200

    Added a bunch of tests for animated moves

--- packages/inferno-animation/src/index.ts
+++ packages/inferno-animation/src/index.ts
@@ -1,6 +1,6 @@
 export { AnimatedComponent } from './AnimatedComponent';
 export { AnimatedMoveComponent } from './AnimatedMoveComponent';
-export { componentDidAppear, componentWillDisappear, AnimationClass } from './animations';
+export { componentDidAppear, componentWillDisappear, componentWillMove, AnimationClass } from './animations';
 export { hasPendingAnimations } from './animationCoordinator';
 
 import { addClassName, clearDimensions, forceReflow, getDimensions, registerTransitionListener, removeClassName, setDimensions, setDisplay } from './utils';
