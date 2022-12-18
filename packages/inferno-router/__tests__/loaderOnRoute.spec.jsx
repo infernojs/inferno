8f4b69a07be718af69e99e078d914a34d7f843fb
Sebastian Ware <sebastian@urbantalk.se>
2022-12-18 10:49:25 +0100

    Moved SSR render test to inferno-server

--- packages/inferno-router/__tests__/loaderOnRoute.spec.jsx
+++ packages/inferno-router/__tests__/loaderOnRoute.spec.jsx
@@ -1,5 +1,4 @@
 import { render } from 'inferno';
-import { renderToString } from 'inferno-server'
 import { BrowserRouter, MemoryRouter, StaticRouter, Route, NavLink, resolveLoaders, useLoaderData, useLoaderError } from 'inferno-router';
 import { createEventGuard } from './testUtils';
 
@@ -402,21 +401,6 @@ describe('A <Route> with loader in a StaticRouter', () => {
 });
 
 describe('Resolve loaders during server side rendering', () => {
-  let container;
-
-  beforeEach(function () {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-
-  afterEach(function () {
-    render(null, container);
-    container.innerHTML = '';
-    document.body.removeChild(container);
-    // Reset history to root
-    history.replaceState(undefined, undefined, '/');
-  });
-
   it('Can resolve with single route', async () => {
     const TEXT = 'bubblegum';
     const Component = (props, { router }) => {
@@ -487,39 +471,5 @@ describe('Resolve loaders during server side rendering', () => {
 
     const result = await resolveLoaders('/flowers/birds', app);
     expect(result).toEqual(loaderData);
-  });
-
-
-  it('SSR renders same result as browser', async () => {
-    const TEXT = 'bubblegum';
-    const Component = (props, { router }) => {
-      const res = useLoaderData(props);
-      return <h1>{res?.message}</h1>
-    }
-
-    const loaderFuncNoHit = async () => { return { message: 'no' }}
-    const loaderFunc = async () => { return { message: TEXT }}
-
-    const loaderData = {
-      '/birds': { res: await loaderFunc() }
-    }
-
-    const routes = [
-        <Route path="/flowers" render={Component} loader={loaderFuncNoHit} />,
-        <Route path="/birds" render={Component} loader={loaderFunc} />,
-        <Route path="/bees" render={Component} loader={loaderFuncNoHit} />,
-    ]
-
-    const initialData = await resolveLoaders('/birds', routes);
-
-    // Render on server
-    const html = renderToString(<StaticRouter location="/birds" loaderData={initialData}>{routes}</StaticRouter>);
-    
-    // Render in browser
-    history.replaceState(undefined, undefined, '/birds');
-    render(<BrowserRouter loaderData={initialData}>{routes}</BrowserRouter>, container);
-
-
-    expect(`<!--!-->${container.innerHTML}<!--!-->`).toEqual(html);
   });
 })
