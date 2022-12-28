f8b6ffc515d175658de26dd1c8a24ae421b3f2a8
Sebastian Ware <sebastian@urbantalk.se>
2022-12-28 08:49:00 +0100

    Removed loader code in Switch since it is handled by Router now

--- packages/inferno-router/src/Switch.ts
+++ packages/inferno-router/src/Switch.ts
@@ -1,9 +1,9 @@
 import { Component, createComponentVNode, VNode } from 'inferno';
 import { matchPath } from './matchPath';
 import { invariant, warning } from './utils';
-import { combineFrom, isArray, isInvalid, isUndefined } from 'inferno-shared';
+import { combineFrom, isArray, isInvalid } from 'inferno-shared';
 import { IRouteProps, Match } from './Route';
-import { RouterContext, TLoader } from './Router';
+import { RouterContext } from './Router';
 
 function getMatch(pathname: string, { path, exact, strict, sensitive, loader, from }, router: { route, initialData?: any }) {
   path ??= from;
@@ -51,32 +51,6 @@ export class Switch extends Component<IRouteProps, SwitchState> {
     }
   }
 
-  private runLoader(loader: TLoader<any, any>, params, request, match, _child) {
-    // TODO: Pass progress callback to loader
-    loader({ params, request })
-      .then((res) => {
-        // TODO: react-router parses json
-        // NOTE: The route stores initialData in state
-        match.initialData = { res };
-        this.setState({ match, _child });
-      })
-      .catch((err) => {
-        // Loaders should throw errors
-        match.initialData = { err };
-        this.setState({ match, _child })
-      });
-  }
-
-  public componentDidMount(): void {
-    const { match, _child } = this.state!;
-    // QUESTION: Is there a better way to invoke this on/after first render?
-    if (!isUndefined(match?.loader) && isUndefined(match?.initialData)) {
-      const params = match.params;
-      const request = undefined;
-      this.runLoader(match.loader!, params, request, match, _child);
-    }
-  }
-
   public componentWillReceiveProps(nextProps: IRouteProps, nextContext: RouterContext): void {
     if (process.env.NODE_ENV !== 'production') {
       warning(
@@ -94,13 +68,6 @@ export class Switch extends Component<IRouteProps, SwitchState> {
     const { location, children } = nextProps;
     const pathname = (location || router.route.location).pathname;
     const { match, _child } = extractFirstMatchFromChildren(pathname, children, router);
-
-    if (match?.loader) {
-      const params = match.params;
-      const request = undefined;
-      this.runLoader(match.loader, params, request, match, _child);
-      return;
-    }
 
     this.setState({ match, _child })
   }
