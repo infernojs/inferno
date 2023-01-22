07cdfcc20328b86fcc16ddd93e5526708e0b9971
Sampo Kivistö <sampo.kivisto@live.fi>
2023-01-22 19:14:45 +0200

    Deleted old standalone babel plugin bundles

--- browser/jsfiddle-integration-babel.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var tag = document.querySelector('script[type="application/javascript"]');
-  if (!tag || tag.textContent.indexOf('window.onload=function(){') !== -1) {
-    alert('Bad JSFiddle configuration, please fork the original Inferno JSFiddle');
-  }
-  tag.setAttribute('type', 'text/babel');
-  tag.textContent = tag.textContent.replace(/^\/\/<!\[CDATA\[/, '');
-})();
