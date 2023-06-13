684057588e20ed9fb0217f4e511e11a6eecea250
dxvladislavvolkov <vladislav.volkov@devexpress.com>
2023-06-13 18:45:08 +0400

    Add THIRD_PARTY_LIBRARY flag

--- packages/inferno/index.esm.js
+++ packages/inferno/index.esm.js
@@ -1,5 +1,5 @@
 export * from './dist/index.esm.js';
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production' || !process.env.THIRD_PARTY_LIBRARY) {
   console.warn('You are running production build of Inferno in development mode. Use dev:module entry point.');
 }
