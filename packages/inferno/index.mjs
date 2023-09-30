ebb13f244cf75e8cb7666c17bb67c2e045092169
Sampo Kivistö <sampo.kivisto@live.fi>
2023-09-30 08:34:19 +0300

    breaking change: Rename inferno ES module bundle extension to .mjs

--- packages/inferno/index.mjs
+++ packages/inferno/index.mjs
@@ -1,4 +1,4 @@
-export * from './dist/index.esm.js';
+export * from './dist/index.mjs';
 
 if (process.env.NODE_ENV !== 'production') {
   console.warn('You are running production build of Inferno in development mode. Use dev:module entry point.');
