551709c7a00c0431302a4479a3741f3d56c2b7c6
Sampo Kivistö <sampo.kivisto@live.fi>
2025-01-22 23:36:16 +0200

    Removed fixtures/packaging, it does not provide value for the time spent

--- fixtures/packaging/rjs/dev/input.js
+++
@@ -1,3 +0,0 @@
-require(['inferno', 'inferno-create-element'], function (Inferno, createElement) {
-  Inferno.render(createElement('h1', null, 'Hello World!'), document.getElementById('container'));
-});
