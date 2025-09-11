d9a6decd7250900ba04ddb776170c09c89386f53
chris <chris@bumblehead.com>
2025-09-11 14:56:11 -0700

    chore: remove single dependency, fs-extra

--- scripts/bundle/move-compiled.js
+++ scripts/bundle/move-compiled.js
@@ -1,11 +1,9 @@
-import { readFileSync } from 'fs';
+import { readFileSync, cpSync } from 'fs';
 import { join } from 'path';
-import { copySync } from 'fs-extra/esm';
 
 const cwd = process.cwd();
 const pkgJSONtext = readFileSync(join(cwd, 'package.json'));
 const pkgJSON = JSON.parse(pkgJSONtext);
-
 if (!pkgJSON.private) {
-  copySync(join(cwd, '../../build/packages/', pkgJSON.name, 'src'), join(cwd, 'tmpDist'), { overwrite: true });
+  cpSync(join(cwd, '../../build/packages/', pkgJSON.name, 'src'), join(cwd, 'tmpDist'), { recursive: true, force: true });
 }
